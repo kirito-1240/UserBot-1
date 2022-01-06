c247fdaf6fa18a4dc7dda581e6e17c9a458b8565
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-07 03:05:18 +0330

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -11,4 +11,4 @@ def load_plugins(plugin_name):
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
     sys.modules["userbot.plugins." + plugin_name] = load
-    print("•UserBot Has Imported : " + plugin_name)
+    print("• UserBot Has Imported : " + plugin_name)
