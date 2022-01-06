4a26f7cb325a220244175863cdd185bd5e7c6fa4
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-07 01:20:23 +0330

    Update EMFV.py

--- userbot/plugins/EMFV.py
+++ userbot/plugins/EMFV.py
@@ -1,6 +1,7 @@
 from userbot import app
 from telethon import events
 from moviepy.editor import VideoFileClip
+import os
 
 @app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.emfv$"))
 async def start(event):
