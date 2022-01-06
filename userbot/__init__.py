30fe356549d8e150c59be3dbb3f2463336a255f2
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-07 00:42:47 +0330

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,17 +1,9 @@
-# By < @xditya >
-# // @BotzHub //
-
 from telethon import TelegramClient
-from decouple import config
-import logging
-import time
+from telethon.sessions import StringSession
+from .Config import Config
 
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
+API_ID = Config.API_ID
+API_HASH = Config.API_HASH
+STRING_SESSION = Config.STRING_SESSION
 
-# Basics
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-
-BotzHub = TelegramClient('BotzHub', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
+app = TelegramClient(StringSession(str(STRING_SESSION)) , API_ID, API_HASH) 
