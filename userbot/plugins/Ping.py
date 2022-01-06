43463e4f00bdefb0c1b4f1b372c73cc466f7e01a
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-07 00:50:31 +0330

    Update Ping.py

--- userbot/plugins/Ping.py
+++ userbot/plugins/Ping.py
@@ -1,1 +1,11 @@
- 10
+from userbot import app
+from telethon import events
+from datetime import datetime
+
+@app.on(events.NewMessage(outgoing=True , pattern="(?i)^/ping$"))
+async def start(event):
+    start = datetime.now()
+    edit = await event.edit("**Pong!!**")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await edit.edit(f"**• Pong!!** - `{ms}`")
