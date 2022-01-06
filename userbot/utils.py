00364be78c7c11fe40bc5d3ba78e6f7ba74df78d
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-07 00:56:41 +0330

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -4,11 +4,11 @@ import importlib
 from pathlib import Path
 
 def load_plugins(plugin_name):
-    path = Path(f"TelethonBot/plugins/{plugin_name}.py")
-    name = "TelethonBot.plugins.{}".format(plugin_name)
+    path = Path(f"userbot/plugins/{plugin_name}.py")
+    name = "userbot.plugins.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["TelethonBot.plugins." + plugin_name] = load
+    sys.modules["userbot.plugins." + plugin_name] = load
     print("•UserBot Has Imported : " + plugin_name)
