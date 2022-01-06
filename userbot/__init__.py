faed1bc6e49a2bae43e956677a7de443e9c06ef5
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-07 02:37:56 +0330

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,9 +1,12 @@
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 from .Config import Config
+import time
 
 API_ID = Config.API_ID
 API_HASH = Config.API_HASH
 STRING_SESSION = Config.STRING_SESSION
 
 app = TelegramClient(StringSession(str(STRING_SESSION)) , API_ID, API_HASH) 
+
+START_TIME = time.time()
