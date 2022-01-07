252259c26703d01664c3e3e88c0b97996fd3b563
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-07 17:45:39 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 import os
 
-print("3")
-os.system("python3 -m userbot")
-quit()
-print("•> UserBot Started!")
-
-os.system("python3 -m inlinebot")
-quit()
-print("•> InlineBot Started!")
+print("• > InlineBot & UserBot Started!")
+os.system("python3 -m userbot & python3 -m inlinebot")
