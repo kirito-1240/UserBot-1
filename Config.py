b3a8cb4620a96ee5c977b1ab7f2fe228b2dd41ef
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-07 16:40:45 +0330

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config(object):
+    API_ID = int(os.environ.get("API_ID"))
+    API_HASH = os.environ.get("API_HASH")
+    STRING_SESSION = os.environ.get("STRING_SESSION")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_GROUP = os.environ.get("BOT_GROUP")
