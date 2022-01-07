01cd04108f37c903c6db2f2579c4699d0b0a1a37
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-07 21:09:42 +0330

    Update Restart.py

--- userbot/plugins/Restart.py
+++ userbot/plugins/Restart.py
@@ -4,10 +4,11 @@ import sys , os
 
 @app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.restart$"))
 async def start(event):
-    event = await event.edit("` Restarting - [ ░░░ ]`")      
-    await event.edit("`Restarting - [ █░░ ]`")
-    await event.edit("`Restarting - [ ██░ ]`")
-    await event.edit("`Restarting - [ ███ ]`")
+    one = "█"
+    two = "░"
+    for i in range(0 , 8):
+        c = 7 - int(i)
+        await event.edit(f"""`• Restarting - [ {one*i}{two*c} ]`""")      
     await event.edit("**• Bot Restarted!**")
     os.execl(sys.executable, sys.executable, *sys.argv)
     quit()
