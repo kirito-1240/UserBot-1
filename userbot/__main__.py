d6e4ade940ac6163480d0832c8ed0ad794ef8942
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-07 16:22:58 +0330

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -7,12 +7,36 @@ from time import sleep
 
 path = "userbot/plugins/*.py"
 files = glob.glob(path)
-send = app.send_message(Config.BOT_GROUP , "**• Starting Import UserBot Plugins ...**")
 for name in files:
     with open(name) as a:
         patt = Path(a.name)
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
+
+
+path = "userbot/inlinebot/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins_inline(plugin_name.replace(".py", ""))
+        
+
+print("Successfully Deployed!")
+
+bot.run_until_disconnected()
+app.start()
+app.run_until_disconnected()
+
+path = "userbot/plugins/*.py"
+files = glob.glob(path)
+send = app.send_message(Config.BOT_GROUP , "**• Starting Import UserBot Plugins ...**")
+sleep(3)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
         app.edit_message(send.chat_id , send.id , f"""`• Successfuly Imported : {plugin_name.replace(".py", "")} From UserBot Plugins!`""")
         sleep(2)
 app.edit_message(send.chat_id , send.id , "**• Successfuly Imported UserBot Plugins!**")
@@ -25,14 +49,7 @@ for name in files:
     with open(name) as a:
         patt = Path(a.name)
         plugin_name = patt.stem
-        load_plugins_inline(plugin_name.replace(".py", ""))
         app.edit_message(send.chat_id , send.id , f"""`• Successfuly Imported : {plugin_name.replace(".py", "")} From InlineBot Plugins!`""")
         sleep(2)
 app.edit_message(send.chat_id , send.id , "**• Successfuly Imported InlineBot Plugins!**")
 
-
-print("Successfully Deployed!")
-
-bot.run_until_disconnected()
-app.start()
-app.run_until_disconnected()
