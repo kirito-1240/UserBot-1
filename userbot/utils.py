621ce5c20dd4049bcf908623481d39d6f72ab504
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-07 16:45:41 +0330

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -12,13 +12,3 @@ def load_plugins(plugin_name):
     spec.loader.exec_module(load)
     sys.modules["userbot.plugins." + plugin_name] = load
     print("• UserBot Has Imported : " + plugin_name)
-
-def load_plugins_inline(plugin_name):
-    path = Path(f"userbot/inlinebot/{plugin_name}.py")
-    name = "userbot.inlinebot.{}".format(plugin_name)
-    spec = importlib.util.spec_from_file_location(name, path)
-    load = importlib.util.module_from_spec(spec)
-    load.logger = logging.getLogger(plugin_name)
-    spec.loader.exec_module(load)
-    sys.modules["userbot.inlinebot." + plugin_name] = load
-    print("• InlineBot Has Imported : " + plugin_name)
