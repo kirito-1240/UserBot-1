250d26478e3cd2d2b8cefe9c8cfeb60bc540d497
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-07 15:23:10 +0330

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -6,7 +6,10 @@ import time
 API_ID = Config.API_ID
 API_HASH = Config.API_HASH
 STRING_SESSION = Config.STRING_SESSION
+BOT_TOKEN = Config.BOT_TOKEN
 
 app = TelegramClient(StringSession(str(STRING_SESSION)) , API_ID, API_HASH) 
+
+bot = TelegramClient('InlineBot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
 START_TIME = time.time()
