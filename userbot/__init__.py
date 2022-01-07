709533205a9d9f0f25c867c680a889144d1c8da4
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-07 09:30:56 +0330

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,7 +1,7 @@
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 from .Config import Config
-import time
+import time , heroku3
 
 API_ID = Config.API_ID
 API_HASH = Config.API_HASH
@@ -10,3 +10,7 @@ STRING_SESSION = Config.STRING_SESSION
 app = TelegramClient(StringSession(str(STRING_SESSION)) , API_ID, API_HASH) 
 
 START_TIME = time.time()
+
+HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[
+            Config.HEROKU_APP_NAME
+        ]
