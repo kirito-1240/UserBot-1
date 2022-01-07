ad570d775fa4708e9dfa27eb60d4e4a0a705e611
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-08 01:38:04 +0330

    Create CodeRunner.py

---
+++ userbot/plugins/CodeRunner.py
@@ -0,0 +1,68 @@
+from userbot import app
+from telethon import events
+import asyncio
+import io
+import os
+import sys
+import traceback
+import time
+
+async def aexec(code , status):
+    message = event = status
+    p = lambda _x: print(_format.yaml_format(_x))
+    reply = await event.get_reply_message()
+    exec(
+        (
+            "async def __aexec(message, event , reply , client , p , chat): "
+            + "".join(f"\n {l}" for l in code.split("\n"))
+        )
+    )
+
+    return await locals()["__aexec"](
+        message, event, reply, message.client, p, message.chat_id
+    )
+
+
+@app.on(events.NewMessage(outgoing=True , pattern="(?i)^/run(?:\s|$)([\s\S]*)$"))
+async def CodeRunner(event):
+    if event.text[4:]:
+        cmd = "".join(event.message.message.split(maxsplit=1)[1:])
+    elif not event.reply_to == None:
+        reply = await event.get_reply_message()
+        cmd = reply.text
+    else:
+        return await event.edit("`• What Should I Run ?`")
+    edit = await event.edit("`• Running ...`")
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+    try:
+        await aexec(cmd , event)
+    except Exception:
+        exc = traceback.format_exc()
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success!"
+    output = f"""**✮  Your Code : ** \n `/run\n\n{cmd}`\n\n**✮  Result : ** \n `{evaluation}`"""
+    await edit.delete()
+    if len(str(output)) < 4000:
+        await app.send_message(event.chat_id , output)
+    else:
+        output = f"""✮  Your Code : \n /run\n\n{cmd}\n\n✮  Result : \n {evaluation}"""
+        with open('Result.txt', 'w') as f:
+            f.write(str(output))
+            f.close()
+        await app.send_file(event.chat_id, "Result.txt" , caption="**• Your Code Reply Exceeded One Message! \n • See The Answer In The File!**" , reply_to=event.message.id)
+        os.remove("./Result.txt")
