f67798263e87a14b8a78d9f55628ba1fe77f7004
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-07 10:29:06 +0330

    Update AMOV.py

--- userbot/plugins/AMOV.py
+++ userbot/plugins/AMOV.py
@@ -25,22 +25,12 @@ async def start(event):
             await app.download_media(media , "amov.mp4")
             audio = AudioFileClip("smfv.mp3")
             video = VideoFileClip("amov.mp4")
-            print((int(audio.duration) - int(video.duration)) , int(audio.duration))
-            if int(audio.duration) > int(video.duration):
-                music = AudioFileClip("smfv.mp3").cutout((int(audio.duration) - int(video.duration)) , int(audio.duration))
-                music.write_audiofile("newsmfv.mp3")
-            if os.path.exists("newsmfv.mp3"):
-                audio = AudioFileClip("newsmfv.mp3")
-            else:
-                audio = AudioFileClip("smfv.mp3")
             final = video.set_audio(audio)
             final.write_videofile("newamov.mp4") 
             await edit.delete()
             await app.send_file(event.chat_id , "newamov.mp4" , reply_to=reply.id , voice_note=True , caption="**• Added Coustom Music On This Video!**")
             os.remove("amov.mp4")
             os.remove("newamov.mp4")
-            if os.path.exists("newsmfv.mp3"):
-                os.remove("newsmfv.mp3")
         else:
             await edit.edit("**• Please Reply To Video!**") 
     else:       
