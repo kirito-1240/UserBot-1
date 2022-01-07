7d260dcbf90bf0a8cfadc97e1217591576ed95f3
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-07 16:11:05 +0330

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -7,28 +7,28 @@ from time import sleep
 
 path = "userbot/plugins/*.py"
 files = glob.glob(path)
-send = await app.send_message(Config.BOT_GROUP , "**• Starting Import UserBot Plugins ...**")
+send = app.send_message(Config.BOT_GROUP , "**• Starting Import UserBot Plugins ...**")
 for name in files:
     with open(name) as a:
         patt = Path(a.name)
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
-        await app.edit_message(send.chat_id , send.id , f"""`• Successfuly Imported : {plugin_name.replace(".py", "")} From UserBot Plugins!`""")
+        app.edit_message(send.chat_id , send.id , f"""`• Successfuly Imported : {plugin_name.replace(".py", "")} From UserBot Plugins!`""")
         sleep(2)
-await app.edit_message(send.chat_id , send.id , "**• Successfuly Imported UserBot Plugins!**")
+app.edit_message(send.chat_id , send.id , "**• Successfuly Imported UserBot Plugins!**")
 
 path = "userbot/inlinebot/*.py"
 files = glob.glob(path)
-send = await app.send_message(Config.BOT_GROUP , "**• Starting Import InlineBot Plugins ...**")
+send = app.send_message(Config.BOT_GROUP , "**• Starting Import InlineBot Plugins ...**")
 sleep(3)
 for name in files:
     with open(name) as a:
         patt = Path(a.name)
         plugin_name = patt.stem
         load_plugins_inline(plugin_name.replace(".py", ""))
-        await app.edit_message(send.chat_id , send.id , f"""`• Successfuly Imported : {plugin_name.replace(".py", "")} From InlineBot Plugins!`""")
+        app.edit_message(send.chat_id , send.id , f"""`• Successfuly Imported : {plugin_name.replace(".py", "")} From InlineBot Plugins!`""")
         sleep(2)
-await app.edit_message(send.chat_id , send.id , "**• Successfuly Imported InlineBot Plugins!**")
+app.edit_message(send.chat_id , send.id , "**• Successfuly Imported InlineBot Plugins!**")
 
 
 print("Successfully Deployed!")
