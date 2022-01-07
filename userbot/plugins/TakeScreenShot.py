a6371d08524039d3ca2e41358a99195f8dad6464
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-08 00:03:17 +0330

    Create TakeScreenShot.py

---
+++ userbot/plugins/TakeScreenShot.py
@@ -0,0 +1,14 @@
+from userbot import app
+from telethon import events
+import requests , os
+        
+@app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.scr (.*)$"))
+async def RotateVideo(event):
+    site = str(event.text[4:])
+    edit = await event.edit("`Please Wait ...`")
+    response = requests.get("https://render-tron.appspot.com/screenshot/" + site, stream=True)
+    with open("scr.png" , 'wb') as file:
+        for chunk in response:
+            file.write(chunk)
+    await app.send_file(event.chat_id , "scr.png")
+    os.remove("scr.png")
