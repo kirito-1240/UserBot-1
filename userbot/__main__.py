3f45416785e685d880572336d1d94991e342008f
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-07 15:24:55 +0330

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,7 +1,7 @@
 import glob
 from pathlib import Path
-from userbot.utils import load_plugins
-from . import app
+from userbot.utils import load_plugins , load_plugins_inline
+from . import app , bot
 
 path = "userbot/plugins/*.py"
 files = glob.glob(path)
@@ -10,8 +10,17 @@ for name in files:
         patt = Path(a.name)
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
+path = "inlinebot/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins_inline(plugin_name.replace(".py", ""))
+
 
 print("Successfully Deployed!")
 
 app.start()
 app.run_until_disconnected()
+bot.run_until_disconnected()
