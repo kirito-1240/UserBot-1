1d9985beb86bbac03e3c6a05f2e81a1dc709135b
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-07 20:41:11 +0330

    Update UpdatePlugins.py

--- userbot/plugins/UpdatePlugins.py
+++ userbot/plugins/UpdatePlugins.py
@@ -1,14 +1,14 @@
-from inlinebot import bot
+from userbot import app
 from userbot.utils import load_plugins
-from inlinebot.utils import load_plugins as loadplugins
 from telethon import events
 import sys , os , glob
 from time import sleep
 from pathlib import Path
 
-@bot.on(events.NewMessage(pattern="(?i)^\.upplugins$"))
-async def start(event):
-    event = await event.edit("**• Starting Import UserBot Plugins ...**")      
+@app.on(events.NewMessage(pattern="(?i)^\.upplugins$"))
+async def UpdatePlugins(event):
+    event = await event.edit("**• Starting Import UserBot Plugins ...**")
+    sleep(3)
     path = "userbot/plugins/*.py"
     files = glob.glob(path)
     for name in files:
@@ -19,16 +19,3 @@ async def start(event):
             await event.edit(f"""**• UserBot Has Imported :** ( `{plugin_name.replace(".py", "")}` )""")
             sleep(2)
     await event.edit("**• UserBot Has Imported Plugins!**")
-
-    edit = await event.reply("**• Starting Import InlineBot Plugins ...**")      
-    path = "inlinebot/plugins/*.py"
-    files = glob.glob(path)
-    for name in files:
-        with open(name) as a:
-            patt = Path(a.name)
-            plugin_name = patt.stem
-            loadplugins(plugin_name.replace(".py", ""))
-            await edit.edit(f"""**• InlineBot Has Imported :** ( `{plugin_name.replace(".py", "")}` )""")
-            sleep(2)
-    await edit.edit("**• InlineBot Has Imported Plugins!**")
-
