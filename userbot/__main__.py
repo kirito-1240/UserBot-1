5da858f71424e18e23550cc09667ddc053d025e4
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-08 13:53:22 +0330

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,9 +1,22 @@
-from . import app
+import sys
+from Config import Config
 from userbot.utils import setup_bot
+from userbot.session import app
 
-app.loop.run_until_complete(setup_bot())
+async def startup_process():
+    setup_bot()
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+    print("• Your Userbot Is Officially Working!!!")
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
 
-try:
-    app.run_until_disconnected()
-except ConnectionError:
-    pass
+
+app.loop.run_until_complete(startup_process())
+
+
+if len(sys.argv) not in (1, 3, 4):
+    app.disconnect()
+else:
+    try:
+        app.run_until_disconnected()
+    except ConnectionError:
+        pass
