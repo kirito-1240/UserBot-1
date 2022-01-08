0a4df073e46e79031e8c7411c72a1346bd348d9d
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-08 23:31:02 +0330

    Create CopyMessages.py

---
+++ userbot/plugins/CopyMessages.py
@@ -0,0 +1,18 @@
+from userbot import app
+from telethon import events
+
+@app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.copy$"))
+async def CopyMessages(event):
+    reply = await event.get_reply_message()
+    if reply:
+        if reply.fwd_from:
+            await reply.forward_to(event.chat_id)    
+        elif reply.media and reply.text:
+            await event.reply(reply.text , file=reply.media)    
+        elif reply.media:
+            await  event.reply(file=reply.media)    
+        else:
+            await event.reply(reply.text)
+        
+    else:
+        await event.reply("**• Please Reply To Message For Copy!**")
