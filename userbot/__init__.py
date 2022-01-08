07c0d9055ca7065bc13f3f4157af846129a35bac
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-08 14:08:58 +0330

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,7 +1,29 @@
+from telethon import TelegramClient
+from telethon.sessions import StringSession
 from Config import Config
-from userbot.session import app
-import time
+from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
+import sys , time
 
-app = app
+API_ID = Config.API_ID
+API_HASH = Config.API_HASH
+STRING_SESSION = Config.STRING_SESSION
+VERSION = "1.0.6"
+LOOP = None
+
+try:
+    app = TelegramClient(
+        session = StringSession(str(STRING_SESSION)),
+        api_id = API_ID,
+        api_hash = API_HASH,
+        loop = LOOP,
+        app_version = VERSION,
+        connection = ConnectionTcpAbridged,
+        auto_reconnect = True,
+        connection_retries = None,
+    )
+except Exception as e:
+    print(f"• Error On Create App : {e}")
+    sys.exit()
+
 
 START_TIME = time.time()
