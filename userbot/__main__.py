0ba2d81db7bbaa9ec56ab73ede7a82063ef637c3
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-08 10:53:42 +0330

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,21 +1,7 @@
-import glob
-from pathlib import Path
-from userbot.utils import load_plugins
 from . import app
-from Config import Config
-from time import sleep
+from userbot.utils import setup_bot
 
-path = "userbot/plugins/*.py"
-files = glob.glob(path)
-for name in files:
-    with open(name) as a:
-        patt = Path(a.name)
-        plugin_name = patt.stem
-        try:
-            load_plugins(plugin_name.replace(".py", ""))
-            print(f"""• UserBot Has Imported {plugin_name.replace(".py", "")} Plugin""")
-        except Exception as e:
-            print(f"""• UserBot Can't Import {plugin_name.replace(".py", "")} Plugin - Becuse Of Error {e}""")
+await setup_bot()
 
 app.start()
 app.run_until_disconnected()
