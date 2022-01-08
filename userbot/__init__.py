9a41a6143ea626d22a9c9addca1ac86837db382e
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-08 13:55:24 +0330

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,17 +1,6 @@
-from telethon import TelegramClient
-from telethon.sessions import StringSession
 from Config import Config
-import time
-import sys
-from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
+from userbot.session import app
 
-API_ID = Config.API_ID
-API_HASH = Config.API_HASH
-STRING_SESSION = Config.STRING_SESSION
-VERSION = "1.0.6"
-LOOP = None
-
-
-app = TelegramClient(StringSession(str(STRING_SESSION)) , API_ID, API_HASH, loop=LOOP, app_version=VERSION, connection=ConnectionTcpAbridged, auto_reconnect=True, connection_retries=None)
+app = app
 
 START_TIME = time.time()
