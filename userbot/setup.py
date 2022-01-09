647b2e6205b237676dae05ea1e64280c6b403e4e
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-09 12:42:54 +0330

    Update setup.py

--- userbot/setup.py
+++ userbot/setup.py
@@ -1,1 +1,24 @@
+from userbot import app
+from telethon import events
+from userbot.utils import load_plugins
+from time import sleep
 
+@app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.setup$"))
+async def start(event):
+    edit = await event.edit("**• Starting Setup Plugins ...**")
+    sleep(3)
+    path = "userbot/plugins/*.py"
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as a:
+            patt = Path(a.name)
+            plugin_name = patt.stem
+            try:
+                load_plugins(plugin_name.replace(".py", ""))
+                print(f"""• UserBot Has Imported {plugin_name.replace(".py", "")} Plugin""")
+                await event.reply(f"""**• UserBot Has Imported :** ( `{plugin_name.replace(".py", "")}` )**Plugin!**""")
+            except Exception as e:
+                print(f"""• UserBot Can't Import {plugin_name.replace(".py", "")} Plugin - Becuse Of Error {e}""")
+                await event.reply(f"""**• UserBot Can't Import :** ( `{plugin_name.replace(".py", "")}` ) **Plugin - Becuse Of Error :** ( `{e}` )""")
+    await edit.edit("**• Setup Plugins Completed!**")
+    await app.send_message(event.chat_id , "**• UserBot Has Been Start Now!**")
