4e74a64ce26b6e03f68aa5c1225a093a5a593f09
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-09 11:32:11 +0330

    Update AddAudioOnVideo.py

--- userbot/plugins/AddAudioOnVideo.py
+++ userbot/plugins/AddAudioOnVideo.py
@@ -3,21 +3,22 @@ from telethon import events
 from moviepy.editor import VideoFileClip
 from moviepy.editor import AudioFileClip
 import os
+from userbot.utils import please_wait         
 
 @app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.setaudio$"))
 async def SetAudioForVideo(event):
-    edit = await event.edit("`Please Wait ...`")
+    await please_wait(event)
     reply = await event.get_reply_message()
     if (not event.reply_to == None and reply.document.mime_type == "audio/mpeg") or (not event.reply_to == None and reply.document.mime_type == "audio/ogg"):
         media = reply.media
         await app.download_media(media , "smfv.mp3")
-        await edit.edit("**• Audio Was Set For Added To Videos!**")
+        await event.edit("**• Audio Was Set For Added To Videos!**")
     else:
-        await edit.edit("**• Please Reply To Music!**")
+        await event.edit("**• Please Reply To Music!**")
         
 @app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.addaudio$"))
 async def AddAudioOnVideo(event):
-    edit = await event.edit("`Please Wait ...`")
+    await please_wait(event)
     reply = await event.get_reply_message()
     if os.path.exists("smfv.mp3"):
         if not event.reply_to == None and reply.document.mime_type == "video/mp4":
@@ -27,11 +28,11 @@ async def AddAudioOnVideo(event):
             video = VideoFileClip("amov.mp4")
             final = video.set_audio(audio)
             final.write_videofile("newamov.mp4") 
-            await edit.delete()
+            await event.delete()
             await app.send_file(event.chat_id , "newamov.mp4" , reply_to=reply.id , voice_note=True , caption="**• Added Coustom Audio On This Video!**")
             os.remove("amov.mp4")
             os.remove("newamov.mp4")
         else:
-            await edit.edit("**• Please Reply To Video!**") 
+            await event.edit("**• Please Reply To Video!**") 
     else:       
-            await edit.edit("**• Please Set Audio By Command :** `.safv`") 
+            await event.edit("**• Please Set Audio By Command :** `.safv`") 
