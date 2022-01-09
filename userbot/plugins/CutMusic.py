0f52b8e6f90847d462152800aeab158659de553e
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-09 11:35:41 +0330

    Update CutMusic.py

--- userbot/plugins/CutMusic.py
+++ userbot/plugins/CutMusic.py
@@ -1,22 +1,22 @@
 from userbot import app
 from telethon import events
 import ffmpeg , os
-from userbot.utils import runcmd , convert_bytes
+from userbot.utils import runcmd , convert_bytes , please_wait
         
 @app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.cmusic (\d*) (\d*)$"))
 async def CutMusic(event):
-    edit = await event.edit("`Please Wait ...`")
+    await please_wait(event)
     reply = await event.get_reply_message()
     if not event.reply_to == None and reply.document.mime_type == "audio/mpeg":
         media = reply.media
         async def callback(current, total):
-            await edit.edit(f"""`Downloading ...`\n\n**• Current Size:** ( `{convert_bytes(current)}` )\n**• Total Size:** ( `{convert_bytes(total)}` )""")
+            await event.edit(f"""`Downloading ...`\n\n**• Current Size:** ( `{convert_bytes(current)}` )\n**• Total Size:** ( `{convert_bytes(total)}` )""")
         await app.download_media(media , "cutmusic.mp3" , progress_callback=callback)
-        await edit.edit("**• Download Completed!**\n`Please Wait For Cuting ...`")
+        await event.edit("**• Download Completed!**\n`Please Wait For Cuting ...`")
         time1 = event.text.split(" ")[1]
         time2 = event.text.split(" ")[2]
         await runcmd(f"ffmpeg -i cutmusic.mp3 -ss {time1} -to {time2} -acodec copy cutmusicoutput.mp3")
-        await edit.delete()
+        await event.delete()
         await app.send_file(event.chat_id , "cutmusicoutput.mp3" , reply_to=reply.id  , caption="**• This ShortMusic Was Cuted From This Music!**")
         os.remove("cutmusicoutput.mp3")
         os.remove("cutmusic.mp3")
