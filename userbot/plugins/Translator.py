352a9e3573b341832381d5c5afcffc47bb1dff50
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-09 11:43:35 +0330

    Update Translator.py

--- userbot/plugins/Translator.py
+++ userbot/plugins/Translator.py
@@ -2,13 +2,14 @@ from userbot import app
 from telethon import events
 import os
 from userbot.utils import runcmd
+from userbot.utils import please_wait
 os.system("pip install deep_translator")
 from deep_translator import GoogleTranslator
 from deep_translator.exceptions import LanguageNotSupportedException , NotValidLength
 
 @app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.tr (.*)$"))
 async def Translator(event):
-    edit = await event.edit("`Please Wait ...`")
+    await please_wait(event)
     lang = str(event.text[4:])
     reply = await event.get_reply_message()
     if not event.reply_to == None and reply.media and reply.document.mime_type == "text/plain" and not reply.text:
@@ -19,17 +20,17 @@ async def Translator(event):
         try:
             output = GoogleTranslator(source='auto', target=lang).translate(text)
         except LanguageNotSupportedException:
-            return await edit.edit("**• Language Not Supported!**")
+            return await event.edit("**• Language Not Supported!**")
         except NotValidLength:
-            return await edit.edit("**• Not Valid Text Length!**")
+            return await event.edit("**• Not Valid Text Length!**")
         os.remove("input.txt")
         if len(str(output)) < 4000:
-            await edit.edit(f"""**• Your Text:** \n{text}\n\n**• Translate To {lang}**:\n {output}""")
+            await event.edit(f"""**• Your Text:** \n{text}\n\n**• Translate To {lang}**:\n {output}""")
         else:
             with open('Result.txt', 'w') as f:
                 f.write(f"""• Your Text: \n{text}\n\n• Translate To {lang}:\n {output}""")
                 f.close()
-            await edit.delete()
+            await event.delete()
             await app.send_file(event.chat_id, "Result.txt" , caption="**• See The Answer In The File!**" , reply_to=reply.id)
             os.remove("Result.txt")
     elif not event.reply_to == None and reply.text:
@@ -37,17 +38,17 @@ async def Translator(event):
         try:
             output = GoogleTranslator(source='auto', target=lang).translate(text)
         except LanguageNotSupportedException:
-            return await edit.edit("**• Language Not Supported!**")
+            return await event.edit("**• Language Not Supported!**")
         except NotValidLength:
-            return await edit.edit("**• Not Valid Text Length!**")
+            return await event.edit("**• Not Valid Text Length!**")
         if len(str(output)) < 4000:
-            await edit.edit(f"""**• Your Text:** \n{text}\n\n**• Translate To {lang}**:\n {output}""")
+            await event.edit(f"""**• Your Text:** \n{text}\n\n**• Translate To {lang}**:\n {output}""")
         else:
             with open('Result.txt', 'w') as f:
                 f.write(f"""• Your Text: \n{text}\n\n• Translate To {lang}:\n {output}""")
                 f.close()
-            await edit.delete()
+            await event.delete()
             await app.send_file(event.chat_id, "Result.txt" , caption="**• See The Answer In The File!**" , reply_to=reply.id)
             os.remove("Result.txt")
     else:
-        await edit.edit("**• Please Reply To File Or Message For Translate!**")     
+        await event.edit("**• Please Reply To File Or Message For Translate!**")     
