437ec22eccac882001a73f91e3eb8987546c7c76
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-09 11:37:34 +0330

    Update ExtractAudioFromVideo.py

--- userbot/plugins/ExtractAudioFromVideo.py
+++ userbot/plugins/ExtractAudioFromVideo.py
@@ -2,19 +2,20 @@ from userbot import app
 from telethon import events
 from moviepy.editor import VideoFileClip
 import os
+from userbot.utils import please_wait
 
 @app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.exaudio$"))
 async def ExtractAudioFromVideo(event):
-    edit = await event.edit("`Please Wait ...`")
+    await please_wait(event)
     reply = await event.get_reply_message()
     if not event.reply_to == None and reply.document.mime_type == "video/mp4":
         media = reply.media
         await app.download_media(media , "emfv.mp4")
         clip = VideoFileClip("emfv.mp4")
         clip.audio.write_audiofile("output.mp3")
-        await edit.delete()
+        await event.delete()
         await app.send_file(event.chat_id , "output.mp3" , reply_to=reply.id , voice_note=True , caption="**• Extracted Audio From Video!**")
         os.remove("output.mp3")
         os.remove("emfv.mp4")
     else:
-        await edit.edit("**• Please Reply To Video!**")
+        await event.edit("**• Please Reply To Video!**")
