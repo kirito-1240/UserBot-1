04a9314e2200e1f339b894402d8d48a7a8c6acf7
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-09 16:29:29 +0330

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -5,23 +5,20 @@ import logging , sys
 import importlib
 import glob
 
-path = "userbot/setu*.py"
+print("• Starting Setup Plugins ...")
+path = "userbot/plugins/*.py"
 files = glob.glob(path)
 for name in files:
     with open(name) as a:
         patt = Path(a.name)
         plugin_name = patt.stem
-        plugin_name = plugin_name.replace(".py", "")
-        path = Path(f"userbot/{plugin_name}.py")
-        name = "userbot.{}".format(plugin_name)
-        spec = importlib.util.spec_from_file_location(name, path)
-        load = importlib.util.module_from_spec(spec)
-        load.logger = logging.getLogger(plugin_name)
-        spec.loader.exec_module(load)
-        sys.modules["userbot." + plugin_name] = load
-        print("• UserBot Has Imported Setup Plugin")
-        print("• Please Send ( .setup ) Command On Telegram To Setup Plugins")
-
+        try:
+            load_plugins(plugin_name.replace(".py", ""))
+            print(f"""• UserBot Has Imported {plugin_name.replace(".py", "")} Plugin""")
+        except Exception as e:
+            print(f"""• UserBot Can't Import {plugin_name.replace(".py", "")} Plugin - Becuse Of Error {e}""")
+    print("• Setup Plugins Completed!")
+    print("• UserBot Has Been Start Now!")
 
 app.start()
 app.run_until_disconnected()
