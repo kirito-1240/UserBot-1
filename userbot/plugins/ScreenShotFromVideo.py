9c1a2736fc90257f581a8cf5342b363aca61bf77
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-09 11:40:11 +0330

    Update ScreenShotFromVideo.py

--- userbot/plugins/ScreenShotFromVideo.py
+++ userbot/plugins/ScreenShotFromVideo.py
@@ -4,21 +4,22 @@ from moviepy.editor import VideoFileClip
 import os , random , time
 from userbot.utils import take_screen_shot , convert_bytes
 from moviepy.editor import VideoFileClip
+from userbot.utils import please_wait
         
 @app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.scrv ?(\d*)?$"))
 async def ScreenShotFromVideo(event):
-    edit = await event.edit("`Please Wait ...`")
+    await please_wait(event)
     reply = await event.get_reply_message()
     if not event.reply_to == None and reply.document.mime_type == "video/mp4":
         media = reply.media
         async def callback(current, total):
-            await edit.edit(f"""`Downloading ...`\n\n**• Current Size:** ( `{convert_bytes(current)}` )\n**• Total Size:** ( `{convert_bytes(total)}` )""")
+            await event.edit(f"""`Downloading ...`\n\n**• Current Size:** ( `{convert_bytes(current)}` )\n**• Total Size:** ( `{convert_bytes(total)}` )""")
         await app.download_media(media , "screenshotvideo.mp4" , progress_callback=callback)
-        await edit.edit("**• Download Completed!**\n`Please Wait For Taking ...`")
+        await event.edit("**• Download Completed!**\n`Please Wait For Taking ...`")
         if event.text[5:]:
             time = int(event.text[5:])
             await take_screen_shot("screenshotvideo.mp4" , time , "screenshot.jpg")
-            await edit.delete()
+            await event.delete()
             await app.send_file(event.chat_id , "screenshot.jpg" , reply_to=reply.id  , caption=f"**• This Photo Was Taken In** (`{time}`) **From This Video!**")
             os.remove("screenshot.jpg")
             os.remove("screenshotvideo.mp4")
@@ -31,10 +32,10 @@ async def ScreenShotFromVideo(event):
                 time = random.choice(rand)
                 await take_screen_shot("screenshotvideo.mp4" , time , f"screenshot{i}.jpg")
                 list.append(f"screenshot{i}.jpg")
-            await edit.delete()
+            await event.delete()
             await app.send_file(event.chat_id , list , reply_to=reply.id  , caption="**• This Photos Was Taken From This Video!**")
             for name in list:
                 os.remove(f"{name}")
             os.remove("screenshotvideo.mp4")            
     else:
-        await edit.edit("**• Please Reply To Video!**")
+        await event.edit("**• Please Reply To Video!**")
