daf8f8ce800b81eb8d119b70796eb45d2dfbdb5a
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-09 12:25:07 +0330

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,7 +1,17 @@
-from userbot.utils import setup_plugins
+from userbot.utils import load_plugins
 from userbot import app
 
-setup_plugins()
+
+path = "userbot/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins(plugin_name.replace(".py", ""))
+        print("• UserBot Has Imported Setup Plugin")
+        print("• Please Send ( .setup ) Command On Telegram To Setup Plugins")
+
 
 app.start()
 app.run_until_disconnected()
