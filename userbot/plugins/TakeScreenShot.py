afbd72273cf760bdf420be5f7a466c049a1153d6
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-09 11:42:08 +0330

    Update TakeScreenShot.py

--- userbot/plugins/TakeScreenShot.py
+++ userbot/plugins/TakeScreenShot.py
@@ -1,14 +1,16 @@
 from userbot import app
 from telethon import events
 import requests , os
+from userbot.utils import please_wait
         
 @app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.scr (.*)$"))
 async def TakeScreenShot(event):
     site = str(event.text[4:])
-    edit = await event.edit("`Please Wait ...`")
+    await please_wait(event)
     response = requests.get("https://render-tron.appspot.com/screenshot/" + site, stream=True)
     with open("scr.png" , 'wb') as file:
         for chunk in response:
             file.write(chunk)
+    await event.delete()
     await app.send_file(event.chat_id , "scr.png")
     os.remove("scr.png")
