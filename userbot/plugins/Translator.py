c4144514a5db6785c49b319d589e8b527f70fdcc
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-09 10:07:54 +0330

    Create Translator.py

---
+++ userbot/plugins/Translator.py
@@ -0,0 +1,40 @@
+from userbot import app
+from telethon import events
+import os
+from deep_translator import GoogleTranslator
+
+@app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.tr (.*)$"))
+async def Translator(event):
+    edit = await event.edit("`Please Wait ...`")
+    lang = event.text[3:]:
+    reply = await event.get_reply_message()
+    if reply.media and reply.document.mime_type == "text/plain" and not reply.text:
+        media = reply.media
+        await app.download_media(media , "input.txt")
+        file = open("input.txt")
+        text = file.read()
+        output = GoogleTranslator(source='auto', target=lang).translate(text)
+        os.remove("input.txt")
+        if len(str(output)) < 4000:
+            await edit.edit(f"""**• Your Text:** \n( `{text}` )\n\n**• Translate To** `{lang}`:\n ( `{output}` )""")
+        else:
+            with open('Result.txt', 'w') as f:
+                f.write(f"""**• Your Text:** \n( `{text}` )\n\n**• Translate To** `{lang}`:\n ( `{output}` )""")
+                f.close()
+            await edit.delete()
+            await app.send_file(event.chat_id, "Result.txt" , caption="**• See The Answer In The File!**" , reply_to=reply.id)
+            os.remove("Result.txt")
+    elif reply.text:
+        text = reply.text
+        output = GoogleTranslator(source='auto', target=lang).translate(text)
+        if len(str(output)) < 4000:
+            await edit.edit(f"""**• Your Text:** \n( `{text}` )\n\n**• Translate To** `{lang}`:\n ( `{output}` )""")
+        else:
+            with open('Result.txt', 'w') as f:
+                f.write(f"""**• Your Text:** \n( `{text}` )\n\n**• Translate To** `{lang}`:\n ( `{output}` )""")
+                f.close()
+            await edit.delete()
+            await app.send_file(event.chat_id, "Result.txt" , caption="**• See The Answer In The File!**" , reply_to=reply.id)
+            os.remove("Result.txt")
+    else:
+        await edit.edit("**• Please Reply To File Or Message For Translate!**")     
