2cd492aa3956f269786e641f2be99c4a78c5e4ea
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-11 00:39:15 +0330

    Update ScreenShotFromVideo.py

--- userbot/plugins/ScreenShotFromVideo.py
+++ userbot/plugins/ScreenShotFromVideo.py
@@ -12,7 +12,7 @@ async def ScreenShotFromVideo(event):
         await event.edit("**• Download Completed!**\n`Please Wait For Taking ...`")
         if event.text[5:]:
             time = int(event.text[5:])
-            await take_screen_shot("screenshotvideo.mp4" , time , "screenshot.jpg")
+            take_screen_shot("screenshotvideo.mp4" , time , "screenshot.jpg")
             await event.delete()
             await app.send_file(event.chat_id , "screenshot.jpg" , reply_to=reply.id  , caption=f"**• This Photo Was Taken In** (`{time}`) **From This Video!**")
             os.remove("screenshot.jpg")
@@ -24,7 +24,7 @@ async def ScreenShotFromVideo(event):
             for i in range(0, 10):
                 rand = range(0 , int(duration))
                 time = random.choice(rand)
-                await take_screen_shot("screenshotvideo.mp4" , time , f"screenshot{i}.jpg")
+                take_screen_shot("screenshotvideo.mp4" , time , f"screenshot{i}.jpg")
                 list.append(f"screenshot{i}.jpg")
             await event.delete()
             await app.send_file(event.chat_id , list , reply_to=reply.id  , caption="**• This Photos Was Taken From This Video!**")
