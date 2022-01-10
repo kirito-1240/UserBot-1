daafe612b970e3d3f144776abb70ec0a94362620
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-10 23:25:28 +0330

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,12 +1,11 @@
-from userbot import *
-from Config import *
-from userbot.utils import *
-from telethon import *
-from moviepy.editor import *
-from moviepy.editor import *
-from datetime import *
-from PIL import *
-from pathlib import *
-from deep_translator import *
-from deep_translator.exceptions import *
+from userbot import app , START_TIME
+from Config import Config
+from userbot.utils import bash , restart_app , load_plugins , convert_bytes , media_type , ytvideo_info , take_screen_shot , runcmd
+from telethon import events
+from moviepy.editor import VideoFileClip , AudioFileClip
+from datetime import datetime
+from PIL import Image
+from pathlib import Path
+from deep_translator import GoogleTranslator
+from deep_translator.exceptions import NotValidLength , LanguageNotSupportedException
 import random , requests , os , asyncio , io , sys , traceback , time , ffmpeg , io , shutil
