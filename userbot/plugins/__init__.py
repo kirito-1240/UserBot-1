271f84dc0f2c5992b0df29ab3a6a0eb9ea6be4e7
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-10 23:14:56 +0330

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,3 +1,12 @@
 from userbot import *
 from Config import *
 from userbot.utils import *
+from telethon import *
+from moviepy.editor import *
+from moviepy.editor import *
+from datetime import *
+from PIL import *
+from pathlib import *
+from deep_translator import *
+from deep_translator.exceptions import *
+import random , requests , os , asyncio , io , sys , traceback , time , ffmpeg , io , shutil
