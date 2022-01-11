deeabce0226aa6fc2d61d345946e53847f530d2b
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-11 15:09:17 +0330

    Update TakeScreenShot.py

--- userbot/plugins/TakeScreenShot.py
+++ userbot/plugins/TakeScreenShot.py
@@ -2,12 +2,12 @@ from . import *
 
 @app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.scr (.*)$"))
 async def TakeScreenShot(event):
-    site = str(event.text[4:])
+    site = event.pattern_match.group(1)
     await event.edit("`• Please Wait ...`")
     driver = webdriver.Chrome("chromedriver")
     driver.get(site)
     screenshot = driver.save_screenshot("site.png")
     driver.quit()
     await event.delete()
-    await app.send_file(event.chat_id , "site.png")
+    await app.send_file(event.chat_id , "site.png" , caption=f"**• ScreenShot From Site:** ( `{site}` )")
     os.remove("site.png")
