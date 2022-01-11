fc5b0e5fef812a4e7b4fe7a465066120ea15706a
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-11 09:57:29 +0330

    Update SendAction.py

--- userbot/plugins/SendAction.py
+++ userbot/plugins/SendAction.py
@@ -1,15 +1,21 @@
 from . import *
 
-@app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.saction (\S*) (\d*)$"))
+@app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.saction ?(\S*)? ?(\d*)?$"))
 async def SendAction(event):
-    edit = await event.edit("`• Please Wait ...`")
-    action = event.match_pattern.group(1)
-    time = int(event.match_pattern.group(2))
+    edit = await event.edit("• Please Wait ...")
+    if event.pattern_match.group(1):
+        action = event.pattern_match.group(1)
+    else:
+        action = "typing"
+    if event.pattern_match.group(2):
+        time = int(event.pattern_match.group(2))
+    else:
+        time = 30
     try:
         async with app.action(event.chat_id , action):
-            await edit.edit(f"`• Sending {action} Action For {time}s On This Chat ...`")
+            await edit.edit(f"• Sending {action} Action For {time}s On This Chat ...")
             await sleep(time)
             await edit.delete()
-            await app.send_message(event.chat_id , "**• Send Action Completed!**")
+            await app.send_message(event.chat_id , "• Send Action Completed!")
     except ValueError:
-        await edit.edit("**• Your Action Not Found!**")
+        await edit.edit("• Your Action Not Found!")
