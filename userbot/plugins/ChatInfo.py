f658b27d782c9b1267b345d9a45b3c725281ad63
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-12 00:49:35 +0330

    Update ChatInfo.py

--- userbot/plugins/ChatInfo.py
+++ userbot/plugins/ChatInfo.py
@@ -1,9 +1,14 @@
 from . import *
 
-@app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.cinfo$"))
-async def start(event):   
+@app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.cinfo ?(.*)?$"))
+async def start(event):
     await event.edit("`• Please Wait . . .`")
-    end = datetime.now()
-    chat = await event.get_chat()
-    result = await get_chat_info(chat , event)
+    try:
+        if event.pattern_match.group(1):
+            chat = await app.get_entity(event.pattern_match.group(1))
+        else:
+            chat = await event.get_chat()
+    except ValueError:
+        return await event.edit("**• Your Input Chat/Channel Id Invalid!**")
+    result = await get_chat_info(chat)
     await event.edit(str(result))
