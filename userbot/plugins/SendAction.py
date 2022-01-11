621094ab5b59391d8aebca88e5a65efc0cd1e510
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-11 09:49:02 +0330

    Update SendAction.py

--- userbot/plugins/SendAction.py
+++ userbot/plugins/SendAction.py
@@ -1,17 +1,10 @@
 from . import *
 
-@app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.saction ?(\S*)? ?(\d*)?$"))
+@app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.saction (\S*) (\d*)$"))
 async def SendAction(event):
     edit = await event.edit("`• Please Wait ...`")
-    if event.match_pattern.group(1):
-        action = await event.match_pattern.group(1)
-    else:
-        action = "typing"
-    if event.match_pattern.group(2):
-        time = await event.match_pattern.group(2)
-        time = int(time)
-    else:
-        time = 30
+    action = event.match_pattern.group(1)
+    time = int(event.match_pattern.group(2))
     try:
         async with app.action(event.chat_id , action):
             await edit.edit(f"`• Sending {action} Action For {time}s On This Chat ...`")
