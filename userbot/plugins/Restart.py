666e62b7c6bc7b976a2d24c588d6f6bacea422cb
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-12 13:59:25 +0330

    Update Restart.py

--- userbot/plugins/Restart.py
+++ userbot/plugins/Restart.py
@@ -4,11 +4,12 @@ from . import *
 async def start(event):
     one = "█"
     two = "░"
-    for i in range(0 , 8):
-        c = 7 - int(i)
+    for i in range(0 , 10):
+        c = 9 - int(i)
         await event.edit(f"""`• Restarting - [ {one*i}{two*c} ]`""")      
     await event.edit("**• Bot Restarted!**")
-    restart_app()
     await bash("git pull && pip3 install -r requirements.txt")
+    await bash("git push -u heroku master")
     os.execl(sys.executable, sys.executable, "-m", "userbot")
+    restart_app()
 
