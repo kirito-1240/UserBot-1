d82d43d1cc7fbb9c67242fb58825d0633529e793
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-12 11:03:31 +0330

    Update TagMembers.py

--- userbot/plugins/TagMembers.py
+++ userbot/plugins/TagMembers.py
@@ -2,18 +2,20 @@ from . import *
 
 @app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.tag ?(.*)?$"))
 async def TagMembers(event):
-    st = await event.edit("`• Starting Tag Members ...`")
-    if event.pattern_match.group(1):
-        chat_id = await app.get_entity(event.pattern_match.group(1))
-        chat_id = chat_id.id
-    else:    
-        chat_id = event.chat_id
-    list = []
-    async for user in app.iter_participants(chat_id):
-        if user.username:
-            list.append("@" + user.username)
-    for user in chunks(list , 30):
-        edit = await app.send_message(event.chat_id , "\n".join(user))
-        await app.send_message('me' , "\n".join(user))
-        await edit.edit("**__• Tag Members!**__")
-    await st.edit(f"""**__• Tag Members Completed!**__""")
+    try:
+        st = await event.edit("`• Starting Tag Members ...`")
+        if event.pattern_match.group(1):
+            chat_id = await app.get_entity(event.pattern_match.group(1))
+            chat_id = chat_id.id
+        else:    
+            chat_id = event.chat_id
+        list = []
+        async for user in app.iter_participants(chat_id):
+            if user.username:
+                list.append("@" + user.username)
+        for user in chunks(list , 5):
+            edit = await app.send_message(event.chat_id , "\n".join(user))
+            await edit.edit("****__• Tag Members!**__**")
+        await st.edit("**• Tag Members Completed!**")
+    except ValueError:
+        await event.edit("**• Your Chat ID Invalid!**")
