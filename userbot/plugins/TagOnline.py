3f3f9e73016fe6a08a89315cca5d5277163201bd
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-12 13:31:42 +0330

    Update TagOnline.py

--- userbot/plugins/TagOnline.py
+++ userbot/plugins/TagOnline.py
@@ -4,13 +4,12 @@ from . import *
 async def TagOnlines(event):
     st = await event.edit("`• Starting Tag Onlines ...`")
     list = []
-    async for user in app.iter_participants(chat_id , filter=ChannelParticipantsRecent):
+    async for user in app.iter_participants(event.chat_id , filter=ChannelParticipantsRecent):
         if not user.bot and not user.deleted:
             if user.username:
                 list.append("@" + user.username)
             else:
                 list.append(f"[user.first_name](tg://user?id={user.id})")
     for user in chunks(list , 5):
-        edit = await app.send_message(event.chat_id , "\n".join(user))
-        await edit.edit("****__• Tag Onlines!**__**")
+        edit = await app.send_message(event.chat_id , "\n•".join(user))
     await st.edit("**• Tag Onlines Completed!**")
