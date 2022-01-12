eb6e5298c51cc124119f35e8dcfd0ac8e03fdf01
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-12 13:32:11 +0330

    Update TagMembers.py

--- userbot/plugins/TagMembers.py
+++ userbot/plugins/TagMembers.py
@@ -4,13 +4,12 @@ from . import *
 async def TagMembers(event):
     st = await event.edit("`• Starting Tag Members ...`")
     list = []
-    async for user in app.iter_participants(chat_id):
+    async for user in app.iter_participants(event.chat_id):
         if not user.bot and not user.deleted:
             if user.username:
                 list.append("@" + user.username)
             else:
                 list.append(f"[user.first_name](tg://user?id={user.id})")
     for user in chunks(list , 5):
-        edit = await app.send_message(event.chat_id , "\n".join(user))
-        await edit.edit("****__• Tag Members!**__**")
+        edit = await app.send_message(event.chat_id , "\n•".join(user))
     await st.edit("**• Tag Members Completed!**")
