6d7f0bbafbd248e42cf9fd6ab5fa693c4ca75406
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-12 10:20:08 +0330

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,6 +1,6 @@
 from userbot import app , START_TIME
 from Config import Config
-from userbot.utils import get_chat_info , convert_time , bash , restart_app , load_plugins , convert_bytes , take_screen_shot , runcmd
+from userbot.utils import chunks , get_chat_info , convert_time , bash , restart_app , load_plugins , convert_bytes , take_screen_shot , runcmd
 from telethon import events
 from moviepy.editor import VideoFileClip , AudioFileClip
 from datetime import datetime
