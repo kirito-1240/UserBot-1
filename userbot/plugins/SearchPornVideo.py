d525db858ed103b684db9b4a790d4edf9d9a51cd
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-13 12:42:57 +0330

    Create SearchPornVideo.py

---
+++ userbot/plugins/SearchPornVideo.py
@@ -0,0 +1,16 @@
+from . import *
+
+@app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.spornv (.*)$"))
+async def SearchPornVideo(event):
+    await event.edit("`• Starting Tag Admins ...`")
+    keyword = str(event.pattern_match.group(1))
+    client = pornhub.PornHub(keyword)
+    await event.delete()
+    for video in client.getVideos(5):
+        caption = f"""
+**• Name:** ( `{video['name']}` )
+**• Url:** ( [Click Me!]({video['url']}) )
+**• Duration:** ( `{video['duration']}` )
+**• Rating:** ( `{video['rating']}` )
+"""
+        await app.send_file(event.chat_id , video["background"] , caption=caption)
