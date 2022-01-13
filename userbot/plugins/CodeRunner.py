f46b7df232641ebfdfeb011b25e3ad5bad041acc
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-13 23:18:39 +0330

    Update CodeRunner.py

--- userbot/plugins/CodeRunner.py
+++ userbot/plugins/CodeRunner.py
@@ -1,36 +1,17 @@
 from . import *
 
 async def runner(code , event):
-    message = event
-    p = lambda _x: print(_format.yaml_format(_x))
-    reply = await event.get_reply_message()
-    exec((
-            """
-async def coderunner(message , event , reply , client , p , chat): """
-            + "".join(f"\n {l}" for l in code.split("\n"))
-        ))
+    local = lambda _x: print(_format.yaml_format(_x))
+    exec("async def coderunner(event , local , chat_id): "+ "".join(f"\n {l}" for l in code.split("\n")))
+    return await locals()["coderunner"](event , local , event.chat.id)
 
-    return await locals()["coderunner"](
-        message, event, reply, message.client, p, message.chat_id
-    )
-
-
-@app.on(events.NewMessage(outgoing=True , pattern="(?i)^/run(?:\s|$)([\s\S]*)$"))
-async def CodeRunner(event):
-    await event.edit("`• Please Wait ...`")
+@app.on_message(filters.me & filters.regex("(?i)^/run(?:\s|$)([\s\S]*)$"))
+async def CodeRunner(client , event):
+    await event.edit_text("`• Running . . .`")
     if event.text[4:]:
-        cmd = "".join(event.message.message.split(maxsplit=1)[1:])
-    elif not event.reply_to == None:
-        reply = await event.get_reply_message()
-        if reply.media and reply.document.mime_type == "text/plain":
-            media = reply.media
-            await app.download_media(media , "input.txt")
-            file = open("input.txt")
-            cmd = file.read()
-        else:
-            cmd = reply.text
+        cmd = "".join(event.text.split(maxsplit=1)[1:])
     else:
-        return await event.edit("`• What Should I Run ?`")
+        return await event.edit_text("`• What Should I Run ?`")
     old_stderr = sys.stderr
     old_stdout = sys.stdout
     redirected_output = sys.stdout = io.StringIO()
@@ -56,11 +37,11 @@ async def CodeRunner(event):
     output = f"""**✮  Your Code : ** \n `/run\n\n{cmd}`\n\n**✮  Result : ** \n `{evaluation}`"""
     await event.delete()
     if len(str(output)) < 4000:
-        await app.send_message(event.chat_id , output)
+        await app.send_message(event.chat.id , output)
     else:
         output = f"""✮  Your Code : \n /run\n\n{cmd}\n\n✮  Result : \n {evaluation}"""
         with open('Result.txt', 'w') as f:
             f.write(str(output))
             f.close()
-        await app.send_file(event.chat_id, "Result.txt" , caption="**• Your Code Reply Exceeded One Message! \n • See The Answer In The File!**" , reply_to=event.message.id)
+        await app.send_document(event.chat.id, "Result.txt" , caption="**• Your Code Reply Exceeded One Message! \n • See The Answer In The File!**")
         os.remove("./Result.txt")
