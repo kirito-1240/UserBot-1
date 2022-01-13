fe62e6442ba52d0c6a78f634fac767c601e26175
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-13 23:02:25 +0330

    Update Ping.py

--- userbot/plugins/Ping.py
+++ userbot/plugins/Ping.py
@@ -1,10 +1,10 @@
 from . import *
 
-@app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.ping$"))
-async def start(event):
+@app.on_message(filters.me , filters.regex("(?i)^\.ping$"))
+async def Ping(event):
     start = datetime.now()
-    edit = await event.edit("**Pong!!**")
+    edit = await event.edit_text("**Pong!!**")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     uptime = convert_time(time.time() - START_TIME)
-    await edit.edit(f"**• Pong!!** `{ms}`\n**• Uptime :** `{uptime}`")
+    await edit.edit_text(f"**• Pong!!** `{ms}`\n**• Uptime :** `{uptime}`")
