87a1eebbc1116e9442aedf052372e3b1e1988e11
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-14 09:44:04 +0330

    Update ScreenShotFromVideo.py

--- userbot/plugins/ScreenShotFromVideo.py
+++ userbot/plugins/ScreenShotFromVideo.py
@@ -5,23 +5,23 @@ async def ScreenShotFromVideo(client , event):
     await event.edit_text("`• Please Wait ...`")
     if event.reply_to_message and event.reply_to_message.video:
         media = event.reply_to_message.video
-        media = await app.download_media(media)
+        await app.download_media(media , "./inputscr.jpg")
         await event.edit_text("**• Download Completed!**\n`Please Wait For Taking ...`")
         if event.text[5:]:
             time = int(event.text[5:])
-            take_screen_shot(media , time , "screenshot.jpg")
+            take_screen_shot("./inputscr.jpg" , time , "screenshot.jpg")
             await event.delete()
             await app.send_photo(event.chat.id , "screenshot.jpg" , reply_to_message_id=event.reply_to_message.message_id  , caption=f"**• This Photo Was Taken In** ( `{time}` ) **From This Video!**")
             os.remove("screenshot.jpg")
-            os.remove(media)
+            os.remove("./inputscr.jpg")
         else:
             list = []
-            video = VideoFileClip(media)
+            video = VideoFileClip("./inputscr.jpg")
             duration = video.duration
             for i in range(0, 10):
                 rand = range(0 , int(duration))
                 time = random.choice(rand)
-                take_screen_shot(media , time , f"screenshot{i}.jpg")
+                take_screen_shot("./inputscr.jpg" , time , f"screenshot{i}.jpg")
                 list.append(f"screenshot{i}.jpg")
             await event.delete()
             list2 = []
@@ -30,6 +30,6 @@ async def ScreenShotFromVideo(client , event):
             await app.send_media_group(event.chat.id , list2 , reply_to_message_id=event.reply_to_message.message_id)
             for name in list:
                 os.remove(name)
-            os.remove(media)            
+            os.remove("./inputscr.jpg")            
     else:
         await event.edit_text("**• Please Reply To Video!**")
