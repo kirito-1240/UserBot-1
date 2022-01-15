ed3c1787723d4760531266d066d9c0b1d98b0f4d
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-15 14:07:22 +0330

    Update RemoveBG.py

--- userbot/plugins/RemoveBG.py
+++ userbot/plugins/RemoveBG.py
@@ -3,8 +3,8 @@ from . import *
 @app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.rmbg$"))
 async def RemoveBG(event):
     await event.edit("`• Please Wait ...`")
-    if not event.reply_to == None and event.reply_to_message.photo:
-        media = event.reply_to_message.photo
+    if not event.reply_to == None and reply.media.photo:
+        media = reply.media.photo
         media = await app.download_media(media)
         response = requests.post("https://api.remove.bg/v1.0/removebg" , files={'image_file': open(media , "rb")} , data={'size': 'auto'} , headers={'X-Api-Key': Config.RMBG_API_KEY})
         file = open("outrem.png" , "wb")
@@ -13,10 +13,10 @@ async def RemoveBG(event):
         img.save("outrem.webp", "webp")
         img.seek(0)
         await event.delete()
-        await app.send_document(event.chat.id , "outrem.png" , reply_to_message_id=event.reply_to_message.message_id , file_name="Remove.png" , caption="**• Removed Background From Your Photo!**")
-        await app.send_sticker(event.chat.id , open("outrem.webp", "rb") , reply_to_message_id=event.reply_to_message.message_id)
+        await app.send_file(event.chat.id , "outrem.png" , reply_to=reply.id , file_name="Remove.png" , caption="**• Removed Background From Your Photo!**")
+        await app.send_file(event.chat.id , open("outrem.webp", "rb") , reply_to=reply.id)
         os.remove("outrem.png")
         os.remove(media)
         os.remove("outrem.webp")
     else:
-        await event.edit_text("**• Please Reply To Photo!**")
+        await event.edit("**• Please Reply To Photo!**")
