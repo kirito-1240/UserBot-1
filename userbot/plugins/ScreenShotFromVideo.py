f72092f8eb0191e63de89f210f470a00f7032939
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-15 14:13:13 +0330

    Update ScreenShotFromVideo.py

--- userbot/plugins/ScreenShotFromVideo.py
+++ userbot/plugins/ScreenShotFromVideo.py
@@ -3,15 +3,15 @@ from . import *
 @app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.scrv ?(\d*)?$"))
 async def ScreenShotFromVideo(event):
     await event.edit("`• Please Wait ...`")
-    if event.reply_to_message and event.reply_to_message.video:
-        media = event.reply_to_message.video
-        await app.download_media(media , "./inputscr.mp4")
-        await event.edit_text("**• Download Completed!**\n`Please Wait For Taking Shots ...`")
+    if not event.reply_to == None and reply.document.mime_type == "video/mp4":
+        media = reply.media
+        await app.download_media(media , "inputscr.mp4")
+        await event.edit("**• Download Completed!**\n`Please Wait For Taking Shots ...`")
         if event.text[5:]:
             time = int(event.text[5:])
             await take_screen_shot("inputscr.mp4" , time , "screenshot.jpg")
             await event.delete()
-            await app.send_photo(event.chat.id , "screenshot.jpg" , reply_to_message_id=event.reply_to_message.message_id  , caption=f"**• This Photo Was Taken In** ( `{time}` ) **From This Video!**")
+            await app.send_file(event.chat_id , "screenshot.jpg" , reply_to=reply.id , caption=f"**• This Photo Was Taken In** ( `{time}` ) **From This Video!**")
             os.remove("screenshot.jpg")
             os.remove("inputscr.jpg")
         else:
@@ -24,12 +24,9 @@ async def ScreenShotFromVideo(event):
                 await take_screen_shot("inputscr.mp4" , time , f"screenshot{i}.jpg")
                 list.append(f"screenshot{i}.jpg")
             await event.delete()
-            list2 = []
-            for name in list:
-                list2.append(InputMediaPhoto(name))
-            await app.send_media_group(event.chat.id , list2 , reply_to_message_id=event.reply_to_message.message_id)
+            await app.send_file(event.chat_id , list , reply_to=reply.id , caption"**• This ScreenShots Takes From This Video!**")
             for name in list:
                 os.remove(name)
             os.remove("inputscr.mp4")            
     else:
-        await event.edit_text("**• Please Reply To Video!**")
+        await event.edit("**• Please Reply To Video!**")
