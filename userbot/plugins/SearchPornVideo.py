04bf056e337c717d0dfe73990c324f089b4da444
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-15 13:59:59 +0330

    Update SearchPornVideo.py

--- userbot/plugins/SearchPornVideo.py
+++ userbot/plugins/SearchPornVideo.py
@@ -1,9 +1,9 @@
 from . import *
 
-@app.on_message(filters.me & filters.regex("(?i)^\.spornv (.*)$"))
-async def SearchPornVideo(client , event):
-    await event.edit_text("`• Please Wait . . .`")
-    keyword = str(event.text[8:])
+@app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.spornv (.*)$"))
+async def SearchPornVideo(event):
+    await event.edit("`• Please Wait . . .`")
+    keyword = str(event.pattern_match.group(1))
     client = pornhub.PornHub(keyword)
     await event.delete()
     for video in client.getVideos(5):
@@ -13,4 +13,4 @@ async def SearchPornVideo(client , event):
 **• Duration:** ( `{video['duration']}` )
 **• Rating:** ( `{video['rating']}` )
 """
-        await app.send_photo(event.chat.id , video["background"] , caption=caption)
+        await app.send_file(event.chat_id , video["background"] , caption=caption)
