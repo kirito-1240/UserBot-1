88f9cbdb90989c4ef8d00099cb3f19425b63e2a9
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-16 15:29:16 +0330

    Update AllaDownloader.py

--- userbot/plugins/AllaDownloader.py
+++ userbot/plugins/AllaDownloader.py
@@ -1,13 +1,17 @@
 from . import *
 
-@app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.alla (.*)$"))
+@app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.alla (.*) ?(240|480|720)?$"))
 async def AllaDownloader(event):
     await event.edit("`• Please Wait . . .`")
+    if event.pattern_match.group(2):
+        format = str(event.pattern_match.group(2))
+    else:
+        format = "480"
     link = str(event.pattern_match.group(1))
     if not re.search("(?i)^https://alaatv.com/c/(\d*)$" , link):
         await event.edit("**• Your Link Is Invalid!**")
         return
-    url , title , thumb , desc = get_alla_video(link , "480")
+    url , title , thumb , desc = get_alla_video(link , format)
     r = requests.get(url , allow_redirects=True)
     open('allavideo.mp4', 'wb').write(r.content)
     r = requests.get(thumb , allow_redirects=True)
