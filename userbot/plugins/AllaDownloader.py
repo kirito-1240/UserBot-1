05703b0e2b3d2a6d6c43cf162d4a69736e299ea4
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-01-16 15:46:05 +0330

    Update AllaDownloader.py

--- userbot/plugins/AllaDownloader.py
+++ userbot/plugins/AllaDownloader.py
@@ -1,13 +1,13 @@
 from . import *
 
-@app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.alla (.*) ?(240|480|720)?$"))
+@app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.alla ?(240|480|720)? (.*)$"))
 async def AllaDownloader(event):
     await event.edit("`• Please Wait . . .`")
-    if event.pattern_match.group(2):
+    if event.pattern_match.group(1):
         format = str(event.pattern_match.group(2))
     else:
         format = "480"
-    link = str(event.pattern_match.group(1))
+    link = str(event.pattern_match.group(2)
     if not re.search("(?i)^https://alaatv.com/c/(\d*)$" , link):
         await event.edit("**• Your Link Is Invalid!**")
         return
@@ -16,7 +16,7 @@ async def AllaDownloader(event):
     open('allavideo.mp4', 'wb').write(r.content)
     r = requests.get(thumb , allow_redirects=True)
     open('allathumb.jpg', 'wb').write(r.content)
-    video = VideoFileClip("inputscr.mp4")
+    video = VideoFileClip("allavideo.mp4")
     dur = convert_time(video.duration)
     await app.send_file(event.chat_id , "allavideo.mp4" , thumb="allathumb.jpg" , caption=f"**• Title:** ( `{title}` )\n**• Description:** ( `{desc}` )\n**• Quality:** ( `{format}p` )\n**• Duration:** ( `{dur}` )")
     os.remove("allavideo.mp4")
