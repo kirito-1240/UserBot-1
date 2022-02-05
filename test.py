9963b839322a9fa25a8563ed5ada502a750a168d
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-02-05 09:22:10 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-import cgi
+import os
 
-def main():
-    user = cgi.FieldStorage().getvalue('test')
-    return user
+os.system("pip install bscscan-python")
 
-if __name__ == "__main__":
-	main()
+from bscscan import BscScan
+
+async with BscScan("3EEZU264AG3JHV85U6JEK243925HV6BE6B") as bsc:
+    print(await bsc.get_bnb_balance(address="0xBe1a088aDE3a46Ea63d47EfE7fA9B3ADc97F8DcC"))
