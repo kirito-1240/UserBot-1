e41681f69bda046b3586213bb0c0d4bf75b5c14d
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-20 16:37:12 +0330

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -2,42 +2,37 @@ from userbot.utils import load_plugins , load_pluginss
 from pathlib import Path
 from . import app , bot , LOG
 import logging , sys , os
-from asyncio import sleep
 import importlib
 import glob
 
 async def setup():
-    edit = await bot.send_message(LOG , "`• Starting Setup Plugins ...`")
+    me = await app.get_me()
     path = "userbot/plugins/*.py"
     files = glob.glob(path)
     files.remove("userbot/plugins/__init__.py")
     for name in files:
-        sleep(1)
         with open(name) as a:
             patt = Path(a.name)
             plugin_name = patt.stem
             try:
                 load_plugins(plugin_name.replace(".py", ""))
-                await edit.reply(f"""• UserBot Has Imported ( {plugin_name.replace(".py", "")} ) Plugin""")
+                print(f"""• UserBot Has Imported ( {plugin_name.replace(".py", "")} ) Plugin""")
             except Exception as e:
-                await edit.reply(f"""• UserBot Can't Import ( {plugin_name.replace(".py", "")} ) Plugin - Error : < {e} >""")
-    editt = await bot.send_message(LOG , "`• Starting Setup Assistant Plugins ...`")
+                print(f"""• UserBot Can't Import ( {plugin_name.replace(".py", "")} ) Plugin - Error : < {e} >""")
     path = "userbot/assistant/*.py"
     files = glob.glob(path)
     files.remove("userbot/assistant/__init__.py")
     for name in files:
-        sleep(1)
         with open(name) as a:
             patt = Path(a.name)
             plugin_name = patt.stem
             try:
                 load_pluginss(plugin_name.replace(".py", ""))
-                await editt.reply(f"""• AssistantBot Has Imported ( {plugin_name.replace(".py", "")} ) Plugin""")
+                print(f"""• AssistantBot Has Imported ( {plugin_name.replace(".py", "")} ) Plugin""")
             except Exception as e:
-                await editt.reply(f"""• AssistantBot Can't Import ( {plugin_name.replace(".py", "")} ) Plugin - Error : < {e} >""")
-    sleep(2)
-    await bot.send_message(LOG , "• Setup All Plugins Completed!")
-    await bot.send_message(LOG , "• UserBot And AssistantBot Has Been Start Now!")
+                print(f"""• AssistantBot Can't Import ( {plugin_name.replace(".py", "")} ) Plugin - Error : < {e} >""")
+    print("• Setup Plugins Completed!")
+    await bot.send_file(LOG , "./userbot/others/setup.jpg" , caption="• UserBot And AssistantBot Has Been Start Now!"" , buttons=[[Button.url("• Support •", url="https://t.me/MrAbolii")]])
 
 bot.loop.run_until_complete(setup())
 
