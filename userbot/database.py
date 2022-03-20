5d82f61b69a8ef58f7e90bb05879aa46e0be691e
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-21 01:06:11 +0330

    Update database.py

--- userbot/database.py
+++ userbot/database.py
@@ -1,28 +1,25 @@
-import os
-import sys
+import os , sys
+from Config import Config
 os.system("pip install pymongo")
 from pymongo import MongoClient
 
-class MongoDB:
+DBClient = MongoClient(Config.MongoDB_URL)
+DB = DBClient["UserBot"]
+    
+def get_keys(self):
+    return DB.list_collection_names()
 
-    def __init__(mongo_url):
-        DBClient = MongoClient(mongo_url)
-        DB = DBClient["UserBot"]
+def set_key(key , value):
+    if key in get_keys():
+        DB[key].replace_one({key: value})
+    else:
+        DB[key].insert_one({key: value})
 
-    def keys(self):
-        return DB.list_collection_names()
-
-    def set_key(key , value):
-        if key in keys():
-            DB[key].replace_one({key: value})
-        else:
-            DB[key].insert_one({key: value})
-
-    def del_key(key):
-        if key in keys():
-            DB.drop_collection(key)
+def del_key(key):
+    if key in get_keys():
+        DB.drop_collection(key)
             
-    def get_key(key):
-        if key in keys():
-            value = DB[key].find_one(key)
-            return value
+def get_key(key):
+    if key in get_keys():
+        value = DB[key].find_one(key)
+        return value
