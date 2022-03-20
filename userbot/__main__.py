8df33c9caf1a440fe89c198b4fbe5b1e8cc3ba03
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-20 20:26:17 +0330

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,4 +1,4 @@
-from userbot.utils import load_plugins , load_pluginss , AddBot
+from userbot.utils import load_plugins , AddBot
 from pathlib import Path
 from telethon import Button
 from . import app , bot , LOG
@@ -8,31 +8,8 @@ import glob
 
 async def setup():
     me = await app.get_me()
-    path = "userbot/plugins/*.py"
-    files = glob.glob(path)
-    files.remove("userbot/plugins/__init__.py")
-    for name in files:
-        with open(name) as a:
-            patt = Path(a.name)
-            plugin_name = patt.stem
-            try:
-                load_plugins(plugin_name.replace(".py", ""))
-                print(f"""• UserBot Has Imported ( {plugin_name.replace(".py", "")} ) Plugin""")
-            except Exception as e:
-                print(f"""• UserBot Can't Import ( {plugin_name.replace(".py", "")} ) Plugin - Error : < {e} >""")
-    path = "userbot/assistant/*.py"
-    files = glob.glob(path)
-    files.remove("userbot/assistant/__init__.py")
-    for name in files:
-        with open(name) as a:
-            patt = Path(a.name)
-            plugin_name = patt.stem
-            try:
-                load_pluginss(plugin_name.replace(".py", ""))
-                print(f"""• AssistantBot Has Imported ( {plugin_name.replace(".py", "")} ) Plugin""")
-            except Exception as e:
-                print(f"""• AssistantBot Can't Import ( {plugin_name.replace(".py", "")} ) Plugin - Error : < {e} >""")
-    print("• Setup Plugins Completed!")
+    load_plugins("plugins")
+    load_plugins("assistant")
     await AddBot()
     if me.username:
         username = "@" + me.username
