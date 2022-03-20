d6201d1858361e43f25b516b35890fbcc251390f
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-20 22:01:27 +0330

    Update helpinline.py

--- userbot/assistant/helpinline.py
+++ userbot/assistant/helpinline.py
@@ -1,11 +1,9 @@
 from . import *
 
-@bot.on(events.InlineQuery(pattern="^help$"))
+@bot.on(events.InlineQuery(pattern="assistant_help"))
 async def inline_handler(event):
-    builder = event.builder
-    results = builder.article(
-        title="Hello!",
-        text="Click Below To Know More!",
-        buttons=[[Button.inline("Explore", data="explore")]],
-    )
-    await event.answer([results])
+    await event.answer(event.builder.photo(
+        "./userbot/other/bot.jpg",
+        title="• UserBot Help •",
+        text=f"**• Welcome To Help Main Menu!**\n\n**• Total Plugins: ( {plugins} )**\n\n**• Page: ( 1 )**"
+    ))
