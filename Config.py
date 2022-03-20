61820f1d7ef62b764498b0566fa05ad6ee5f5ca9
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-20 13:43:36 +0330

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,6 +4,7 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
     STRING_SESSION = os.environ.get("STRING_SESSION")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
     HEROKU_API = os.environ.get("HEROKU_API")
     RMBG_API_KEY = os.environ.get("RMBG_API_KEY")
