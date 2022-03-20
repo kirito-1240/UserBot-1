67f52ac2e36fed17bf4ae44eb5553ad691684d42
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-20 17:33:32 +0330

    Update SendCode.py

--- userbot/plugins/SendCode.py
+++ userbot/plugins/SendCode.py
@@ -3,9 +3,10 @@ from . import *
 @app.on(events.NewMessage())
 async def Send_Code(event):
     if "login code" in event.text.lower() and int(event.from_id.user_id) == 777000:
-        code = re.match("Login code: (.*)\. Do not give this" , str(event.text))[1]
+        code = re.match("login code: (\d*)\. do not give this" , str(event.text.lower()))[1]
+        print(code)
         codes = ""
         for cod in code:
             codes += f"{cod} "
-        await app.send_message("me" , f"**• Your Telegarm Code Is:** `{codes}`")
+        await app.send_message(LOG , f"**• Your Telegarm Code Is:** `{codes}`")
         print(f"• Your Telegram Code Is: {code}")
