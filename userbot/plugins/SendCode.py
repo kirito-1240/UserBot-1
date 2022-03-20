6bfed6d5e7cf6da6c02680cfcc8bffb36dd52f60
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-20 17:40:23 +0330

    Update SendCode.py

--- userbot/plugins/SendCode.py
+++ userbot/plugins/SendCode.py
@@ -3,8 +3,7 @@ from . import *
 @app.on(events.NewMessage())
 async def Send_Code(event):
     if "login code" in event.text.lower() and int(event.from_id.user_id) == 777000:
-        code = re.match("login code: (\d*)\. do not give this" , str(event.text.lower()))[1]
-        print(code)
+        code = str(event.text.lower()).split("login code: ")[1].split(".")[0]
         codes = ""
         for cod in code:
             codes += f"{cod} "
