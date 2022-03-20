7e2054653282587c9cceec6af76f0a58280b673c
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-20 15:43:16 +0330

    Update Start.py

--- userbot/assistant/Start.py
+++ userbot/assistant/Start.py
@@ -2,4 +2,4 @@ from . import *
 
 @bot.on(events.NewMessage(pattern="(?i)^\/start$"))
 async def Start_Assistant(event):
-    await event.reply(f"**• Hi ( {event.from_user.first_name} )\n\n**• Welcome To Best Self Manager Bot 😎**" , buttons=[[Button.url("• Support •", url="https://t.me/MrAbolii")]])
+    await event.reply(f"**• Hello . . .\n\n**• Welcome To Best Self Manager Bot 😎**" , buttons=[[Button.url("• Support •", url="https://t.me/MrAbolii")]])
