5db2e12e2ba08af93d796af8f9bce8f55452727b
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-20 12:59:55 +0330

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.system("pip install bscscan-python")
-
-from bscscan import BscScan
-
-async with BscScan("3EEZU264AG3JHV85U6JEK243925HV6BE6B") as bsc:
-    print(await bsc.get_bnb_balance(address="0xBe1a088aDE3a46Ea63d47EfE7fA9B3ADc97F8DcC"))
