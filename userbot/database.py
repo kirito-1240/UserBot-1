4270dd4f0f1f3043eea84f674670ba654e77f4ce
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-21 01:49:41 +0330

    Update database.py

--- userbot/database.py
+++ userbot/database.py
@@ -6,6 +6,7 @@ from redis import Redis
 DB = Redis(
             host="redis-15411.c251.east-us-mz.azure.cloud.redislabs.com:15411",
             password="wIYq4gH5lxQmGVgyk24LZiihAeMNVkVQ",
+            port=15411,
             decode_responses=True,
             socket_timeout=5,
             retry_on_timeout=True,
