4e8c33a755224dd001d81a7056f438e9c84c8329
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-21 02:20:19 +0330

    Update start.py

--- userbot/assistant/start.py
+++ userbot/assistant/start.py
@@ -1,5 +1,8 @@
-from . import *
+from userbot import bot
+from telethon import events , Button
+from userbot.database.botusers import add_user
 
 @bot.on(events.NewMessage(pattern="(?i)^\/start$"))
 async def Start(event):
+    add_user(str(event.from_id.user_id))
     await event.reply(f"**• Hello . . .**\n\n**• Welcome To Best Self Manager Bot 😎**" , buttons=[[Button.url("• Support •", url="https://t.me/MrAbolii")]])
