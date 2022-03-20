754acd5e9256d17a2635dcf894ec773fe3954f59
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-20 13:38:00 +0330

    Update __init__.py

--- userbot/assistant/__init__.py
+++ userbot/assistant/__init__.py
@@ -1,1 +1,19 @@
-.
+from telethon import TelegramClient
+from Config import Config
+import sys
+import time
+
+API_ID = Config.API_ID
+API_HASH = Config.API_HASH
+BOT_TOKEN = Config.BOT_TOKEN
+
+try:
+    bot = TelegramClient(
+        "UserBotAssistant",
+        API_ID,
+        API_HASH,
+        bot_token=BOT_TOKEN,
+    )
+except Exception as e:
+    print(f"• Error On Create App Assistant: {e}")
+    sys.exit()
