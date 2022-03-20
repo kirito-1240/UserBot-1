307510f74faa68a1f350fe49d64acfeb7974d837
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-20 22:53:14 +0330

    Update helpinline.py

--- userbot/assistant/helpinline.py
+++ userbot/assistant/helpinline.py
@@ -1,11 +1,11 @@
 from . import *
 
-@bot.on(events.InlineQuery(pattern="userbot_help"))
+@bot.on(events.InlineQuery(pattern="help"))
 async def inline_handler(event):
     plugins = int(len(glob.glob("userbot/plugins/*.py"))) - 1
     list = glob.glob("userbot/plugins/*.py")
     list.remove("userbot/plugins/__init__.py")
-    buttons = create_buttons(list)
+    buttons = list(create_buttons(list))
     await event.answer([event.builder.article(
         title="• UserBot Help •",
         text=f"**• Welcome To Help Main Menu!**\n\n**• Total Plugins: ( {plugins} )**\n\n**• Page: ( 1 )**",
