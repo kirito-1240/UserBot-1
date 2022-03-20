c74776740f920a02bfadd13effeb084716306da2
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-21 01:38:35 +0330

    Update database.py

--- userbot/database.py
+++ userbot/database.py
@@ -1,25 +1,18 @@
 import os , sys
 from Config import Config
-os.system("pip install pymongo[srv]")
-from pymongo import MongoClient
+os.system("pip install redis")
+from redis import Redis
 
-DBClient = MongoClient(Config.MongoDB_URL)
-DB = DBClient["UserBot"]
-    
-def get_keys():
-    return DB.list_collection_names()
-
-def set_key(key , value):
-    if key in get_keys():
-        DB[key].replace_one({key: value})
-    else:
-        DB[key].insert_one({key: value})
-
-def del_key(key):
-    if key in get_keys():
-        DB.drop_collection(key)
-            
-def get_key(key):
-    if key in get_keys():
-        value = DB[key].find_one(key)
-        return value
+DB = Redis(
+            host="redis-15411.c251.east-us-mz.azure.cloud.redislabs.com:15411",
+            password="wIYq4gH5lxQmGVgyk24LZiihAeMNVkVQ",
+            port=None,
+            decode_responses=True,
+            socket_timeout=5,
+            retry_on_timeout=True,
+        )
+        
+set_key = DB.set
+get_key = DB.get
+get_keys = DB.keys
+del_key = DB.delete
