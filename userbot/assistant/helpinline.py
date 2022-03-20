e5a3696b7f810c4a444fcc9574db186d412dc56b
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-20 22:33:33 +0330

    Update helpinline.py

--- userbot/assistant/helpinline.py
+++ userbot/assistant/helpinline.py
@@ -5,24 +5,10 @@ async def inline_handler(event):
     plugins = int(len(glob.glob("userbot/plugins/*.py"))) - 1
     list = glob.glob("userbot/plugins/*.py")
     list.remove("/userbot/plugins/__init__.py")
-    list = chunks(list , 10)
+    list = chunks(list , 10)[0]
     buttons = create_buttons(list)
     await event.answer([event.builder.article(
         title="• UserBot Help •",
         text=f"**• Welcome To Help Main Menu!**\n\n**• Total Plugins: ( {plugins} )**\n\n**• Page: ( 1 )**",
         buttons=buttons,
     )])
-
-def create_buttons(items):
-    buttons = []
-    list1 = []
-    list2 = []
-    for item in items:
-        list.append([[Button.inline(f"""Audio 🎵 {humanbytes(item['filesize'])}""",
-                                     data=f"a||{item['from_id']}||{item['format_id']}||{item['yturl']}"))
-        if len(list) == 2:
-            buttons.append([list[0] , list[1]])
-            list = []
-    if len(list) > 0:
-        buttons.append([list[0]])          
-    return buttons
