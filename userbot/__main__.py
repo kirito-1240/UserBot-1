2a19a78f8fd5e30da545095d7f1979b5ddcfb5c4
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-20 13:41:49 +0330

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,4 +1,4 @@
-from userbot.utils import load_plugins
+from userbot.utils import load_plugins , load_pluginss
 from pathlib import Path
 from . import app
 from .assistant import bot
@@ -19,8 +19,22 @@ for name in files:
             print(f"""• UserBot Has Imported ( {plugin_name.replace(".py", "")} ) Plugin""")
         except Exception as e:
             print(f"""• UserBot Can't Import ( {plugin_name.replace(".py", "")} ) Plugin - Error : < {e} >""")
-print("• Setup Plugins Completed!")
-print("• UserBot Has Been Start Now!")
+print("• Starting Setup Assistant Plugins ...")
+path = "userbot/plugins/assistant/*.py"
+files = glob.glob(path)
+files.remove("userbot/plugins/assistant/__init__.py")
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        try:
+            load_pluginss(plugin_name.replace(".py", ""))
+            print(f"""• AssistantBot Has Imported ( {plugin_name.replace(".py", "")} ) Plugin""")
+        except Exception as e:
+            print(f"""• AssistantBot Can't Import ( {plugin_name.replace(".py", "")} ) Plugin - Error : < {e} >""")
+
+print("• Setup All Plugins Completed!")
+print("• UserBot And AssistantBot Has Been Start Now!")
 
 app.start()
 bot.start()
