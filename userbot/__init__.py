052f3b4bbd6b6e40ca8553f8f5ca82fe71f4a13d
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-21 01:06:36 +0330

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,6 +1,5 @@
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-from userbot.database import MongoDB
 from Config import Config
 import sys
 import time
@@ -32,4 +31,3 @@ except Exception as e:
 
 START_TIME = time.time()
 LOG = Config.LOG_GROUP
-DB = MongoDB(Config.MongoDB_URL)
