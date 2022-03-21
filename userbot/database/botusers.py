5a401b2765bff97cfc90db2de47c3c116cd49e01
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-21 18:07:33 +0330

    Update botusers.py

--- userbot/database/botusers.py
+++ userbot/database/botusers.py
@@ -5,34 +5,32 @@ from . import BASE, SESSION
 class Bot_Users(BASE):
     __tablename__ = "BOT_USERS"
     user_id = Column(String(14), primary_key=True)
-    date = Column(UnicodeText)
     def __init__(self , user_id , date):
         self.user_id = str(user_id)
-        self.date = date
 
 Bot_Users.__table__.create(checkfirst=True)
 
-def add_user(user_id , date):
-    to_check = info_user(user_id)
+def add_user(user_id):
+    to_check = get_user(user_id)
     if not to_check:
-        user = Bot_Users(str(user_id) , date)
+        user = Bot_Users(str(user_id))
         SESSION.add(user)
         SESSION.commit()
         return True
     rem = SESSION.query(Bot_Users).get(str(user_id))
     SESSION.delete(rem)
     SESSION.commit()
-    user = Bot_Users(str(user_id) , date)
+    user = Bot_Users(str(user_id))
     SESSION.add(user)
     SESSION.commit()
     return True
 
-def info_user(user_id):
+def get_user(user_id):
     try:
-        _result = SESSION.query(Bot_Users).get(str(user_id))
-        if _result:
-            return _result
-        return None
+        result = SESSION.query(Bot_Users).get(str(user_id))
+        if result:
+            return True
+        return False
     finally:
         SESSION.close()
 
