e7c004edc123d66a86b0bc5bad38552b486b7400
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-21 17:59:59 +0330

    Update start.py

--- userbot/assistant/start.py
+++ userbot/assistant/start.py
@@ -1,12 +1,18 @@
 from userbot import bot
+import os
+os.system("pip install pytz")
+import pytz
 from telethon import events , Button
-import time
+import datetime
 from userbot.utils import convert_time
 from userbot.database.botusers import add_user , info_user , get_users
 
 @bot.on(events.NewMessage(pattern="(?i)^\/start$"))
 async def Start(event):
-    add_user(event.peer_id.user_id , round(time.time()))
+    tz = pytz.timezone('Asia/Tehran')
+    now = datetime.datetime.now(tz)
+    time = now.strftime("%Y/%m/%d - %H:%M:%S")
+    add_user(event.peer_id.user_id , time)
     info = await bot.get_entity(event.peer_id.user_id)
     inf = info_user(info.id)
     count = len(get_users())
@@ -16,7 +22,7 @@ async def Start(event):
 **• Welcome To Best Self Manager Bot 😎**
 
 **• Your ID:** ( `{info.id}` )
-**• Join Time:** ( `{convert_time(time.time() - int(inf.date))}` )
+**• Join Time:** ( `{inf.date}` )
 **• Bot Users:** ( `{count}` )
 """
 , buttons=[[Button.url("• Support •", url="https://t.me/MrAbolii")]])
