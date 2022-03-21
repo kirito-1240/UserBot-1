b0bae823965b0acbabd5b9988876a8bf9841862f
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-21 08:21:57 +0330

    Update botusers.py

--- userbot/database/botusers.py
+++ userbot/database/botusers.py
@@ -1,10 +1,16 @@
 from userbot.db import set_key , get_key
 
-def add_user(id):
-    users = get_key("BOT_USERS")
-    if not id in users:
-        users.append(id)
-        set_key("BOT_USERS", users)
+def get_all_users(key):
+    return get_key(key) or []
 
+def is_added(id):
+    return id in get_all_users("BOT_USERS")
+
+def add_user(id):
+    users = get_all_users("BOT_USERS")
+    if not is_added(id):
+        users.append(id)
+        return set_key("BOT_USERS", users):
+    
 def users_count():
     return len(get_key("BOT_USERS"))
