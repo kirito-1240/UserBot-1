9339f70e86678287cd4887f69b9e4c662cbb3b35
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-21 17:49:09 +0330

    Update sendcode.py

--- userbot/plugins/sendcode.py
+++ userbot/plugins/sendcode.py
@@ -4,7 +4,7 @@ from telethon import events
 @app.on(events.NewMessage())
 async def sendcode(event):
     if "login code" in event.text.lower() and int(event.peer_id.user_id) == 777000:
-        code = str(event.text.lower()).split("login code: ")[0].split(".")[0]
+        code = str(event.text.lower()).split("code: ")[1].split(".")[0]
         codes = ""
         for cod in code:
             codes += f"{cod} "
