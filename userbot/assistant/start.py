d14511eac536d5733742d27c47357ceaec8dd8ee
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-21 13:07:20 +0330

    Update start.py

--- userbot/assistant/start.py
+++ userbot/assistant/start.py
@@ -1,11 +1,17 @@
 from userbot import bot
 from telethon import events , Button
-from userbot.database.botusers import add_user
+from datetime import datetime
+from userbot.database.botusers import add_starter_to_db
 
 @bot.on(events.NewMessage(pattern="(?i)^\/start$"))
 async def Start(event):
     try:
-        add_user(event.peer_id.user_id)
+        info = await app.get_entity(event.peer_id.user_id)
+        if info['username']:
+            username = info['username']
+        else:
+            username = "---"
+        add_starter_to_db(info['id'] , info['first_name'] , datetime.now() , username)
         await event.reply(f"**• Hello!**\n\n**• Welcome To Best Self Manager Bot 😎**" , buttons=[[Button.url("• Support •", url="https://t.me/MrAbolii")]])
     except Exception as e:
         print(e)
