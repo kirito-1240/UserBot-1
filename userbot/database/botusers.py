60d8c469ea6d873837565e569347495bd4df8be9
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-21 17:26:08 +0330

    Update botusers.py

--- userbot/database/botusers.py
+++ userbot/database/botusers.py
@@ -2,62 +2,43 @@ from sqlalchemy import Column, String, UnicodeText
 
 from . import BASE, SESSION
 
-class Bot_Starters(BASE):
+class Bot_Users(BASE):
     __tablename__ = "BOT_USERS"
     user_id = Column(String(14), primary_key=True)
-    first_name = Column(UnicodeText)
     date = Column(UnicodeText)
-    username = Column(UnicodeText)
-
-    def __init__(self, user_id, first_name, date, username):
+    def __init__(self , user_id , date):
         self.user_id = str(user_id)
-        self.first_name = first_name
         self.date = date
-        self.username = username
 
-Bot_Starters.__table__.create(checkfirst=True)
+Bot_Users.__table__.create(checkfirst=True)
 
-def add_starter_to_db(
-    user_id,
-    first_name,
-    date,
-    username,
-):
-    to_check = get_starter_details(user_id)
+def add_user(user_id , date):
+    to_check = info_user(user_id)
     if not to_check:
-        user = Bot_Starters(str(user_id), first_name, date, username)
+        user = Bot_Users(str(user_id) , date)
         SESSION.add(user)
         SESSION.commit()
         return True
-    rem = SESSION.query(Bot_Starters).get(str(user_id))
+    rem = SESSION.query(Bot_Users).get(str(user_id))
     SESSION.delete(rem)
     SESSION.commit()
-    user = Bot_Starters(str(user_id), first_name, date, username)
+    user = Bot_Users(str(user_id) , date)
     SESSION.add(user)
     SESSION.commit()
     return True
 
-def del_starter_from_db(user_id):
-    to_check = get_starter_details(user_id)
-    if not to_check:
-        return False
-    rem = SESSION.query(Bot_Starters).get(str(user_id))
-    SESSION.delete(rem)
-    SESSION.commit()
-    return True
-
-def get_starter_details(user_id):
+def info_user(user_id):
     try:
-        _result = SESSION.query(Bot_Starters).get(str(user_id))
+        _result = SESSION.query(Bot_Users).get(str(user_id))
         if _result:
             return _result
         return None
     finally:
         SESSION.close()
 
-def get_all_starters():
+def get_users():
     try:
-        return SESSION.query(Bot_Starters).all()
+        return SESSION.query(Bot_Users).all()
     except BaseException:
         return None
     finally:
