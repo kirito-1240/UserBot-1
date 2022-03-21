a104b12bf7fb47803feac625200ed6d9d04b3907
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-21 09:41:27 +0330

    Update botusers.py

--- userbot/database/botusers.py
+++ userbot/database/botusers.py
@@ -2,13 +2,13 @@ from . import DB
 
 db = DB["BOT_USERS"]
 
-def add_user(user_id):
+async def add_user(user_id):
     if check_user(user_id) is False:
-        db.insert_one({"user_id": user_id})
+        await db.insert_one({"user_id": user_id})
 
-def check_user(user_id):
-    Lol = db.find_one({"user_id": user_id})
+async def check_user(user_id):
+    Lol = await db.find_one({"user_id": user_id})
     return bool(Lol)
 
-def get_all_users():
-    return [s for s in db.find()]
+async def get_all_users():
+    return [s async for s in db.find()]
