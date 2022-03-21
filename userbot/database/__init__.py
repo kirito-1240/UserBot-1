44998a85328bdb3358136ee0607afc86b08883a6
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-21 15:52:27 +0330

    Update __init__.py

--- userbot/database/__init__.py
+++ userbot/database/__init__.py
@@ -1,4 +1,6 @@
 import os
+os.system("pip install sqlalchemy-json")
+os.system("pip install sqlalchemy")
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
