b1492cad6acfbd61c7d42e695b41ccdd668da371
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-21 23:23:10 +0330

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -1,4 +1,7 @@
-from . import *
+from userbot import app , START_TIME
+from telethon import events
+from datetime import datetime
+from userbot.utils import convert_time
 
 @app.on(events.NewMessage(outgoing=True , pattern="(?i)^\.ping$"))
 async def ping(event):
