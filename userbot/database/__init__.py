3efc5dd27de674875295a02a84ba259a11bbe43f
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-21 22:46:24 +0330

    Update __init__.py

--- userbot/database/__init__.py
+++ userbot/database/__init__.py
@@ -1,24 +1,7 @@
 import os
-os.system("pip install sqlalchemy-json")
-os.system("pip install sqlalchemy")
-os.system("pip install psycopg2==2.9.3")
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+os.system("pip install pymongo[srv]")
+from pymongo import MongoClient
 from Config import Config
 
-def start() -> scoped_session:
-    if "postgres://" in Config.DATABASE_URL:
-        database_url = Config.DATABASE_URL.replace("postgres:", "postgresql:")      
-    else:
-        database_url = Config.DATABASE_URL
-    engine = create_engine(database_url)
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
-try:
-    BASE = declarative_base()
-    SESSION = start()
-except AttributeError as e:
-    print("DATABASE_URL Is Not Configured.")
-    print(str(e))
+client = MongoClient(Config.MongoDB_URL , serverSelectionTimeoutMS=5000)
+DB = client["UserBot"]
