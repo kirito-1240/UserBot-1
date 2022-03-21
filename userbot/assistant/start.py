7d2e9caa7de4296755201b9d598b4e5c8ea631b4
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-21 16:05:53 +0330

    Update start.py

--- userbot/assistant/start.py
+++ userbot/assistant/start.py
@@ -7,12 +7,12 @@ from userbot.database.botusers import add_starter_to_db
 async def Start(event):
     try:
         info = await bot.get_entity(event.peer_id.user_id)
-        if info['username']:
-            username = info['username']
+        if info.username:
+            username = info.username
         else:
             username = "---"
-        add_starter_to_db(info['id'] , info['first_name'] , datetime.now() , username)
+        add_starter_to_db(info.id , info.first_name , datetime.now() , username)
         await event.reply(f"**• Hello!**\n\n**• Welcome To Best Self Manager Bot 😎**" , buttons=[[Button.url("• Support •", url="https://t.me/MrAbolii")]])
-    except Exception as e:
-        print(e)
+    except:
+        pass
         
