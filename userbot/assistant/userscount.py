0a3d13da1a917cbf78f7c5f4a09c40d3d4501c3a
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-23 12:05:05 +0430

    Create userscount.py

---
+++ userbot/assistant/userscount.py
@@ -0,0 +1,21 @@
+from userbot import app , bot
+from telethon import events , Button
+from userbot.database.botusers import get_users
+import os
+
+@bot.on(events.NewMessage(pattern="(?i)^\/users$"))
+async def Start(event):
+    id = (await app.get_me()).id
+    if event.from_id.user_id == id:
+        count = len(get_users())
+        users = ""
+        c = 1
+        for user in get_users():
+            users += f"{c}- `{user}`\n"
+        try:
+            await event.reply(f"**• Users Count:** ( `{count}` )\n\n{users}")
+        except:
+            f = open("users.txt" , "w")
+            f.write(f"• Users Count: ( {count} )\n\n{users}")
+            await event.reply("**• List Of Users!**" , file="users.txt")
+            os.remove("users.txt")
