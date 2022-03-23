3c098ad518ffc4b269584feea28cb0222c3d2ba6
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-23 11:49:51 +0430

    Update botusers.py

--- userbot/database/botusers.py
+++ userbot/database/botusers.py
@@ -1,10 +1,16 @@
 from . import DB
 
 def get_users():
-    return [DB.get_key("BOT_USERS")]
+    return DB.get_key("BOT_USERS") or []
 
 def add_user(user_id):
     users = get_users()
-    if user_id not in users:
-        users.append(user_id)
-        return DB.set_key("BOT_USERS", users)
+    if str(type(users)) == "<class 'list'>":
+        if user_id not in users:
+            users.append(user_id)
+            return DB.set_key("BOT_USERS", users)
+    else:
+        users = [get_users()]
+        if user_id not in users:
+            users.append(user_id)
+            return DB.set_key("BOT_USERS", users)
