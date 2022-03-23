48faa51c6d018b61dda1909f64229beda9fe9f93
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-23 17:01:53 +0430

    Update welcome.py

--- userbot/plugins/welcome.py
+++ userbot/plugins/welcome.py
@@ -5,7 +5,7 @@ from userbot.database.welcome import set_welcome , get_welcome , get_chats , del
 @app.on(events.ChatAction)
 async def send_welcome(event):
     msg = get_welcome(event.chat_id)
-    if msg and (event.user_joined or event.user_added) and not (await event.get_user()).bot):
+    if msg and (event.user_joined or event.user_added) and not (await event.get_user()).bot:
         try:
             await event.client.delete_messages(event.chat_id, cws.previous_welcome)
         except:
