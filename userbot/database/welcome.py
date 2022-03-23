377f27a121c681d2144a592f24d97b50472f8991
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-23 17:27:25 +0430

    Update welcome.py

--- userbot/database/welcome.py
+++ userbot/database/welcome.py
@@ -3,14 +3,14 @@ from userbot.database import DB
 def get_chats():
     return DB.get_key("WELCOME_CHATS") or {}
 
-def set_welcome(chat_id , message , media , entity):
+def set_welcome(chat_id , msg_id):
     chats = get_chats()
     if not get_welcome(chat_id):
-        chats.update({chat_id: {"message": message , "media": media , "entities": entity}})
+        chats.update({chat_id: msg_id})
         return DB.set_key("WELCOME_CHATS", chats)
     else:
         DB.del_key(chats[chat_id])
-        chats.update({chat_id: {"message": message , "media": media , "entities": entity}})
+        chats.update({chat_id: msg_id})
         return DB.set_key("WELCOME_CHATS", chats)
 
 def get_welcome(chat_id):
