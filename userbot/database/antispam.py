7e3db66ce222f5ff87077b058ef3117715a62833
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-23 22:18:51 +0430

    Create antispam.py

---
+++ userbot/database/antispam.py
@@ -0,0 +1,42 @@
+from . import DB
+
+def get_limit():
+    return DB.get_key("ANTI_SPAM_LIMIT") or None
+
+def set_limit(lim):
+    DB.set_key("ANTI_SPAM_LIMIT" , lim)
+
+def get_power():
+    return DB.get_key("ANTI_SPAM_POWER") or "off"
+
+def set_power(pow):
+    DB.set_key("ANTI_SPAM_POWER" , pow)
+
+def get_users():
+    return DB.get_key("ANTI_SPAM_USERS") or {}
+
+def add_user(user_id):
+    users = get_users()
+    count = int(get_user(user_id)) + 1
+    if not get_user(user_id):
+        users.update({user_id: count})
+        return DB.set_key("ANTI_SPAM_USERS", users)
+    else:
+        DB.del_key(users[user_id])
+        users.update({user_id: count})
+        return DB.set_key("ANTI_SPAM_USERS", users)
+
+def get_user(user_id):
+    users = get_users()
+    try:
+        return users[user_id]
+    except:
+        return "0"
+
+def del_user(user_id):
+    chats = get_users()
+    if get_user(user_id):
+        del users[user_id]
+        return DB.set_key("ANTI_SPAM_USERS", users)
+    else:
+        return False
