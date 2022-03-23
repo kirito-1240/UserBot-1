bf2babecae2d5ef5fa9f5ceac7d9babc1241158d
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-23 11:29:10 +0430

    Update botusers.py

--- userbot/database/botusers.py
+++ userbot/database/botusers.py
@@ -5,6 +5,12 @@ def get_users():
 
 def add_user(user_id):
     users = get_users()
-    if str(user_id) not in users:
+    if get_user(user_id) is True:
         users.append(str(user_id))
         return DB.set_key("BOT_USERS", users)
+
+def get_user(user_id):
+    users = get_users()
+    if user_id in users:
+        return True
+    return False
