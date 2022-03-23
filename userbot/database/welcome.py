68ca18fede8be04eeb85c377797925d3813dfebb
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-23 15:37:18 +0430

    Update welcome.py

--- userbot/database/welcome.py
+++ userbot/database/welcome.py
@@ -1,10 +1,11 @@
 from . import DB
+import json
 
 def get_chats():
     return DB.get_key("WELCOME_CHATS") or {}
 
 def set_welcome(chat_id , msg):
-    chats = get_chats()
+    chats = json.loads(get_chats())
     if not get_welcome(chat_id):
         chats.update({chat_id: msg})
         return DB.set_key("WELCOME_CHATS", chats)
@@ -14,14 +15,14 @@ def set_welcome(chat_id , msg):
         return udB.set_key("WELCOME_CHATS", chats)
 
 def get_welcome(chat_id):
-    chats = get_chats()
+    chats = json.loads(get_chats())
     try:
         return chats[chat_id]
     except:
         return None
 
 def del_welcome(chat_id):
-    chats = get_chats()
+    chats = json.loads(get_chats())
     if get_welcome(chat_id):
         del chats[chat_id]
         return udB.set_key("WELCOME_CHATS", chats)
