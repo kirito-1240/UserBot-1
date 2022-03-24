3b6448ebdd5f7744b440bdca0b7e740557d37a81
Abolfazl Farzin <96248859+MrAbolii@users.noreply.github.com>
2022-03-24 16:17:53 +0430

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
-    STRING_SESSION = os.environ.get("STRING_SESSION")
+    SESSION = os.environ.get("SESSION")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
     HEROKU_API = os.environ.get("HEROKU_API")
