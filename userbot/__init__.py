00362c83a121bc67871067cfe7a375920889030c
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-25 20:46:25 +0430

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -5,16 +5,14 @@ import sys , time
 import logging
 
 logging.basicConfig(
-    format="[%(name)s - %(asctime)s] : %(message)s",
+    format="[%(asctime)s] : %(message)s",
     level=logging.INFO,
     datefmt="%H:%M:%S",
 )
 
 LOGS = logging.getLogger("BOT")
-LOGSU = logging.getLogger("USER-BOT")
-LOGSA = logging.getLogger("ASSISTANT-BOT")
 START_TIME = time.time()
-LOG = Config.LOG_GROUP
+LOG_GROUP = Config.LOG_GROUP
 API_ID = Config.API_ID
 API_HASH = Config.API_HASH
 SESSION = Config.SESSION
