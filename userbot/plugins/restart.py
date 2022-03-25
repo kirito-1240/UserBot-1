ec4bfc96887091fb653724e327c980576905cbbe
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-25 22:46:16 +0430

    Update restart.py

--- userbot/plugins/restart.py
+++ userbot/plugins/restart.py
@@ -1,6 +1,7 @@
 from userbot import app
 from userbot.utils import restart_app , runcmd
 from telethon import events
+from userbot.database import DB
 import os
 import sys
 
@@ -8,10 +9,11 @@ import sys
 async def restart(event):
     one = "█"
     two = "░"
-    for i in range(0 , 8):
-        c = 7 - int(i)
-        await event.edit(f"""`• Restarting - {one*i}{two*c}`""")      
-    await event.edit("**• Bot Restarted!**")
+    for i in range(0 , 6):
+        c = 5 - int(i)
+        await event.edit(f"""`• Restarting {one*i}{two*c}`""")      
+    await event.edit("**• Bot Restarted!**\n\n`• Please Wait For A Minutes . . .`")
+    DB.set_key("RESTART" , f"{event.chat_id}_{event.id}")
     restart_app()
     await runcmd("git pull -f -q && pip3 install --no-cache-dir -U -q -r requirements.txt")
     os.execl(sys.executable, "python3", "-m", "userbot")
