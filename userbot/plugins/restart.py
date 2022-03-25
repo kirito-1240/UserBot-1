53140d27c94632b9a12bfd1f63b73d661aca4253
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-25 23:11:30 +0430

    Update restart.py

--- userbot/plugins/restart.py
+++ userbot/plugins/restart.py
@@ -13,7 +13,7 @@ async def restart(event):
         c = 5 - int(i)
         await event.edit(f"""`• Restarting {one*i}{two*c}`""")      
     await event.edit("**• Bot Restarted!**\n\n`• Please Wait For A Minutes . . .`")
-    DB.set_key("RESTART" , f"{event.chat_id}||{event.id}")
+    DB.set_key("RESTART" , f"{event.id}||{event.chat_id}")
     restart_app()
     await runcmd("git pull -f -q && pip3 install --no-cache-dir -U -q -r requirements.txt")
     os.execl(sys.executable, "python3", "-m", "userbot")
