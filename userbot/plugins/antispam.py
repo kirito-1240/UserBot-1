678d206b9334b19e387597bb15e8393d5d5ce58a
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-26 03:16:55 +0430

    Update antispam.py

--- userbot/plugins/antispam.py
+++ userbot/plugins/antispam.py
@@ -27,7 +27,7 @@ async def add_users(event):
         add_user(event.peer_id.user_id)
         if int(get_user(event.peer_id.user_id)) == int(get_limit()):
             await event.reply("**• Your Warns Were Exceeded!**\n\n__• You Are Blocked!__")
-            await app(functions.co    add_user(event.peer_id.user_id)ntacts.BlockRequest(id=event.peer_id.user_id))
+            await app(functions.contacts.BlockRequest(id=event.peer_id.user_id))
             info = await app.get_entity(event.peer_id.user_id)
             await app.send_message(LOG_GROUP, f"**• User** [{info.first_name}](tg://user?id={info.id})\n**For Spam In Pv Has Been Blocked You Can Get And Unblock This User!**")
             del_user(event.peer_id.user_id)
