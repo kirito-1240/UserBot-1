403689fa42348727b48603aeef2edc74f2dbb759
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-27 00:41:47 +0430

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -1,6 +1,5 @@
 from userbot import app , START_TIME
 from userbot.events import alien
-from userbot.database import CMDS_HELP
 from datetime import datetime
 from userbot.utils import convert_time
 import time
@@ -13,16 +12,3 @@ async def ping(event):
     ms = (end - start).microseconds / 1000
     uptime = convert_time(time.time() - START_TIME)
     await event.edit(f"**• Pong!!** `{ms}`\n**• Uptime :** `{uptime}`")
-
-
-
-plugin = ((__name__).split(".")[-1])
-CMDS_HELP.update(
-    {
-        plugin:{
-            "info": "To Get Ping And Uptime For Your Bot!",
-            "cmds": {".ping": "To Get Ping And Uptime!"},
-            "exm": [".ping"],
-       }
-    }
-)
