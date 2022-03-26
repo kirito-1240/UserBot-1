01c1211c65b67e2a26cb69bacc851916ba703536
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-26 13:30:52 +0430

    Update userscount.py

--- userbot/assistant/userscount.py
+++ userbot/assistant/userscount.py
@@ -4,20 +4,18 @@ from userbot.events import alien_asst
 from userbot.database.botusers import get_users
 import os
 
-@alien_asst(pattern="(?i)^\/users$")
+@alien_asst(pattern="(?i)^\/users$", me_only=True)
 async def Start(event):
-    id = (await app.get_me()).id
-    if event.peer_id.user_id == id:
-        count = len(get_users())
-        users = ""
-        c = 1
-        for user in get_users():
-            users += f"{c}- `{user}`\n"
-            c += 1
-        try:
-            await event.reply(f"**• Users Count:** ( `{count}` )\n\n{users}")
-        except:
-            f = open("users.txt" , "w")
-            f.write(f"• Users Count: ( {count} )\n\n{users}")
-            await event.reply("**• List Of Users!**" , file="users.txt")
-            os.remove("users.txt")
+    count = len(get_users())
+    users = ""
+    c = 1
+    for user in get_users():
+        users += f"{c}- `{user}`\n"
+        c += 1
+    try:
+        await event.reply(f"**• Users Count:** ( `{count}` )\n\n{users}")
+    except:
+        f = open("users.txt" , "w")
+        f.write(f"• Users Count: ( {count} )\n\n{users}")
+        await event.reply("**• List Of Users!**" , file="users.txt")
+        os.remove("users.txt")
