5f7409f712155382da654594105ffb753996c289
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-26 14:13:53 +0430

    Update tools.py

--- userbot/functions/tools.py
+++ userbot/functions/tools.py
@@ -1,4 +1,10 @@
-import os
+from userbot import LOGS
+import os , re , random
+try:
+    from bs4 import BeautifulSoup
+except:
+    os.system("pip install bs4")
+    from bs4 import BeautifulSoup
 try:
     import aiohttp
 except:
@@ -18,3 +24,43 @@ async def async_searcher(url, post=False, headers=None, params=None, json=None, 
         if real:
             return data
         return await data.text()
+
+random_headers = [
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/72.0.3626.121 Safari/537.36",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100 101 Firefox/22.0",
+    "Mozilla/5.0 (Windows NT 6.1; rv:11.0) Gecko/20100101 Firefox/11.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.5 (KHTML, like Gecko) "
+    "Chrome/19.0.1084.46 Safari/536.5",
+    "Mozilla/5.0 (Windows; Windows NT 6.1) AppleWebKit/536.5 (KHTML, like Gecko) "
+    "Chrome/19.0.1084.46 Safari/536.5",
+    "Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0",
+]
+
+async def google_search(query):
+    query = query.replace(" ", "+")
+    _base = "https://google.com"
+    headers = {
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
+        "User-Agent": random.choice(random_headers),
+    }
+    con = await async_searcher(_base + "/search?q=" + query, headers=headers)
+    soup = BeautifulSoup(con, "html.parser")
+    result = []
+    pdata = soup.find_all("a", href=re.compile("url="))
+    for data in pdata:
+        if not data.find("div"):
+            continue
+        try:
+            result.append(
+                {
+                    "title": data.find("div").text,
+                    "link": data["href"].split("&url=")[1].split("&ved=")[0],
+                    "description": data.find_all("div")[-1].text,
+                }
+            )
+        except BaseException as er:
+            LOGS.info(er)
+    return result
