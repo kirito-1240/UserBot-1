e53afb1b07cbf76a4ce44856c9f1118177b22cc3
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-26 14:02:14 +0430

    Create tools.py

---
+++ userbot/functions/tools.py
@@ -0,0 +1,20 @@
+import os
+try:
+    import aiohttp
+excpet:
+    os.system("pip install aiohttp")
+    import aiohttp
+
+async def async_searcher(url, post=False, headers=None, params=None, json=None, data=None, re_json=False, re_content=False, real=False):
+    async with aiohttp.ClientSession(headers=headers) as client:
+        if post:
+            data = await client.post(url, json=json, data=data)
+        else:
+            data = await client.get(url, params=params)
+        if re_json:
+            return await data.json()
+        if re_content:
+            return await data.read()
+        if real:
+            return data
+        return await data.text()
