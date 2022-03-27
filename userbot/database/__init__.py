7a60f76dfe20f60442fb51aa2ce8a0bb15dc888f
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-27 21:06:07 +0430

    Update __init__.py

--- userbot/database/__init__.py
+++ userbot/database/__init__.py
@@ -1,8 +1,11 @@
 from redis import Redis
+import os
+os.system("pip install pymongo[srv]")
+from pymongo import MongoClient
 from Config import Config
 
-async def get_data(self, key):
-    data = await self.get(str(key))
+def get_data(self, key):
+    data = self.get(str(key))
     if data:
         try:
             data = eval(data)
@@ -11,11 +14,11 @@ async def get_data(self, key):
     return data
 
 class RedisDB:
-    def __init__(self, REDIS_URL, REDIS_PASSWORD, REDIS_PORT):
+    def __init__(self):
         self.db = Redis(
-                host=REDIS_URL,
-                password=REDIS_PASSWORD,
-                port=REDIS_PORT,
+                host=Config.REDIS_URL,
+                password=Config.REDIS_PASSWORD,
+                port=Config.REDIS_PORT,
                 decode_responses=True,
                 socket_timeout=5,
                 retry_on_timeout=True,
@@ -70,4 +73,68 @@ class RedisDB:
             self.del_key(x)
         return True
 
-DB = RedisDB(Config.REDIS_URL, Config.REDIS_PASSWORD, Config.REDIS_PORT)
+class MongoDB:
+    def __init__(self):
+        self.dB = = MongoClient(Config.MongoDB_URL, serverSelectionTimeoutMS=5000)
+        self.db = self.dB["AlienUserBot"]
+        self.recache()
+
+    @property
+    def name(self):
+        return "Mongo"
+
+    @property
+    def usage(self):
+        return self.db.command("dbstats")["dataSize"]
+
+    def recache(self):
+        self.cache = {}
+        for key in self.keys():
+            self.cache.update({key: self.get_key(key)})
+
+    def ping(self):
+        if self.dB.server_info():
+            return True
+
+    def keys(self):
+        return self.db.list_collection_names()
+
+    def set_key(self, key, value):
+        if key in self.keys():
+            self.db[key].replace_one({"id": key}, {"value": str(value)})
+        else:
+            self.db[key].insert_one({"id": key, "value": str(value)})
+        self.cache.update({key: value})
+        return True
+
+    def del_key(self, key):
+        if key in self.keys():
+            try:
+                del self.cache[key]
+            except KeyError:
+                pass
+            self.db.drop_collection(key)
+            return True
+
+    def get_key(self, key):
+        if key in self.cache:
+            return self.cache[key]
+        if key in self.keys():
+            value = get_data(self, key)
+            self.cache.update({key: value})
+            return value
+        return None
+
+    def get(self, key):
+        if x := self.db[key].find_one({"id": key}):
+            return x["value"]
+
+    def flushall(self):
+        self.dB.drop_database("AlienUserBot")
+        self.cache = {}
+        return True
+
+if Config.REDIS_URL:
+    DB = RedisDB()
+else:
+    DB = MongoDB()
