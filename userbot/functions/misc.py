e448a3f13593d6369ed849454f40cd60197822f5
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-28 22:55:54 +0430

    Update misc.py

--- userbot/functions/misc.py
+++ userbot/functions/misc.py
@@ -4,10 +4,10 @@ from userbot.database import DB
 async def add_info_to_db():
     app_info = await app.get_me()
     bot_info = await bot.get_me()
-    DB.set_key("OWNER_ID" , str(app_info.id))
-    DB.set_key("OWNER_NAME" , str(app_info.first_name))
+    DB.set_key("OWNER_ID" , app_info.id)
+    DB.set_key("OWNER_NAME" , app_info.first_name)
     if app_info.username:
-        DB.set_key("OWNER_USERNAME" , str(app_info.username))
-    DB.set_key("ASSISTANT_ID" , str(bot_info.id))
-    DB.set_key("ASSISTANT_NAME" , str(bot_info.first_name))
-    DB.set_key("ASSISTANT_USERNAME" , str(bot_info.username))
+        DB.set_key("OWNER_USERNAME" , app_info.username)
+    DB.set_key("ASSISTANT_ID" , bot_info.id)
+    DB.set_key("ASSISTANT_NAME" , bot_info.first_name)
+    DB.set_key("ASSISTANT_USERNAME" , bot_info.username)
