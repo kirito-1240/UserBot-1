8aa95568af34590987b38941bdc424ed66aa3bd4
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-29 03:03:24 +0430

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,11 +1,10 @@
 import os
 
-class Config(object):
-    API_ID = int(os.environ.get("API_ID")) or None
-    API_HASH = os.environ.get("API_HASH", None)
-    SESSION = os.environ.get("SESSION", None)
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
-    HEROKU_API = os.environ.get("HEROKU_API", None)
-    REDIS_URL = os.environ.get("REDIS_URL", None)
-    REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", None)
+API_ID = int(os.environ.get("API_ID")) or None
+API_HASH = os.environ.get("API_HASH", None)
+SESSION = os.environ.get("SESSION", None)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+HEROKU_API = os.environ.get("HEROKU_API", None)
+REDIS_URL = os.environ.get("REDIS_URL", None)
+REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", None)
