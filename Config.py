4240807352daa7c602b91eab33f70bdb6740375d
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-29 02:58:10 +0430

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,13 +1,12 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    SESSION = os.environ.get("SESSION")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    LOG_GROUP = os.environ.get("LOG_GROUP")
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
-    HEROKU_API = os.environ.get("HEROKU_API")
+    API_ID = int(os.environ.get("API_ID")) or None
+    API_HASH = os.environ.get("API_HASH", None)
+    SESSION = os.environ.get("SESSION", None)
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+    HEROKU_API = os.environ.get("HEROKU_API", None)
     REDIS_URL = os.environ.get("REDIS_URL", None)
     REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", None)
     MongoDB_URL = os.environ.get("MongoDB_URL", None)
