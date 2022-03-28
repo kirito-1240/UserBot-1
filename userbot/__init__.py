c55c4c0c8953dd5344fb0e52a658dec5a7dfc588
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-29 03:05:41 +0430

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,22 +1,16 @@
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-from Config import Config
-from userbot.database import DB
-import sys , time
-from userbot.core.logger import logging
+from userbot.core.logger import LOGS
+import Config
+import time
 
-LOGS = logging.getLogger("Alien-UserBot")
 START_TIME = time.time()
-LOG_GROUP = DB.get_key("LOG_GROUP")
-API_ID = Config.API_ID
-API_HASH = Config.API_HASH
-SESSION = Config.SESSION
 
 try:
     app = TelegramClient(
-        StringSession(str(SESSION)),
-        API_ID,
-        API_HASH,
+        StringSession(str(Config.SESSION)),
+        Config.API_ID,
+        Config.API_HASH,
     ).start()
 except Exception as e:
     LOGS.error(f"• Error On Create App : {e}")
@@ -24,8 +18,8 @@ except Exception as e:
 try:
     bot = TelegramClient(
         "UserBotAssistant",
-        API_ID,
-        API_HASH,
+        Config.API_ID,
+        Config.API_HASH,
     ).start(bot_token=Config.BOT_TOKEN)
 
 except Exception as e:
