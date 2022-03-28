35b52932529ae0bab8e387059d7e6c29935a7add
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-28 14:19:48 +0430

    Update help.py

--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -3,6 +3,7 @@ from userbot.events import alien
 
 @alien(pattern="(?i)^\.help$")
 async def help(event):
+    await event.edit("`• Please Wait . . .`")
     me = await bot.get_me()
     results = await app.inline_query(me.username, "alien_inline_help")
     await results[0].click(event.chat_id)
