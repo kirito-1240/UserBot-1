d4e7b2f89d13a09d6888cca783649cc2c3ccd6b6
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-29 03:17:10 +0430

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -1,5 +1,6 @@
 from . import app, bot
 from userbot.database import DB
+from userbot.core.logger import LOGS
 import Config
 from pathlib import Path
 import os, sys, time, heroku3,logging, math, importlib, glob, shlex, asyncio, functools, re  
