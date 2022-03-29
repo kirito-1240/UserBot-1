c96f6b9d3815edf48ad85b9faae9340e3853fe31
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-30 00:24:28 +0430

    Create uploadtelegraph.py

---
+++ userbot/plugins/uploadtelegraph.py
@@ -0,0 +1,14 @@
+from userbot import app
+from userbot.events import alien
+from html_telegraph_poster import upload_image
+import os
+
+@alien(pattern="(?i)^\.utel$")
+async def uploadtelegraph(event):
+    await event.edit("`• Please Wait . . .`")
+    reply = await event.get_reply_message()
+    if reply:
+        photo = await app.download_media(reply.media)
+        link = upload_image(photo)
+        await event.edit(f"**• Your Telegraph Link:** ( {link} )")
+        os.remove(photo)
