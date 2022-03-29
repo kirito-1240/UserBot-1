7ad689cffe1c8f5beb0af135a777d129f259ad5b
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-30 01:10:52 +0430

    Update photosearch.py

--- userbot/plugins/photosearch.py
+++ userbot/plugins/photosearch.py
@@ -10,10 +10,16 @@ async def photosearch(event):
     await event.edit("`• Please Wait . . .`")
     query = str(event.pattern_match.group(1))
     photos = await unsplashsearch(query)
+    if not photos:
+        return await event.edit("**• Not Found Photo For Your Query!**")
     list = []
-    for x in range(0,9):
+    c = 1
+    for x in photos:
         photo = await download_file(photos[x], f"photo{x}.jpg")
         list.append(photo)
+        c += 1
+        if c == 10:
+            return
     await app.send_file(event.chat_id, list, caption=f"""**• Photos By:** {DB.get_key("OWNER_NAME")}""")
     await event.delete()
     for photo in list:
