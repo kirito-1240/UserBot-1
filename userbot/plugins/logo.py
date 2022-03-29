60ed7e0e377aed58f106bbadfa88d84ca5e98321
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-30 02:23:16 +0430

    Update logo.py

--- userbot/plugins/logo.py
+++ userbot/plugins/logo.py
@@ -25,7 +25,7 @@ async def googlesearch(event):
         LogoMaker.make_logo(photo, text, font, "output.png", fill=random.choice(colors), stroke_width=(stroke_width / 2) + 2, stroke_fill=random.choice(colors))
     except:
         return await event.edit("**• Error, Please Try Again!**")
-    await app.send_file(event.chat_id, "output.png", caption=f"""**• Logo By:** {DB.get_key("OWNER_NAME")}""")
+    await app.send_file(event.chat_id, "output.png", caption=f"""**• Logo By:** {DB.get_key("OWNER")}""")
     await event.delete()
     os.remove("output.png")
     os.remove(photo)
