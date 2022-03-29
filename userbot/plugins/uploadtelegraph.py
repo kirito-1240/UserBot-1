88770439a07bb7eaeb390796f927e630ef7e78e9
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-30 01:32:01 +0430

    Update uploadtelegraph.py

--- userbot/plugins/uploadtelegraph.py
+++ userbot/plugins/uploadtelegraph.py
@@ -1,5 +1,6 @@
 from userbot import app
 from userbot.events import alien
+from userbot.functions.helper import media_type
 from html_telegraph_poster import upload_image
 import os
 
@@ -7,8 +8,10 @@ import os
 async def uploadtelegraph(event):
     await event.edit("`• Please Wait . . .`")
     reply = await event.get_reply_message()
-    if reply:
+    if reply and media_type(reply) == "Photo":
         photo = await app.download_media(reply.media)
         link = upload_image(photo)
-        await event.edit(f"**• Your Telegraph Link:** ( {link} )")
+        await event.edit(f"**• Your Telegraph Link:** ( `{link}` )")
         os.remove(photo)
+    else:
+        await event.edit("**• Please Reply To Photo!**")
