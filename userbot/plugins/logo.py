cf88f1ed10a8c2c04d00c1c87f57d3f49e9eb2e4
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-30 01:52:11 +0430

    Update logo.py

--- userbot/plugins/logo.py
+++ userbot/plugins/logo.py
@@ -10,14 +10,18 @@ async def googlesearch(event):
     await event.edit("`• Please Wait . . .`")
     text = str(event.pattern_match.group(1))
     colors = ["green", "blue", "red", "black", "white", "orange", "yellow", "purple", "pink", "gray"]
-    query = random.choice(["blur", "color", "blur background", "background", "neon", "neon lights", "lights", "light", "city", "car", "wallpaper"])
-    photo = await unsplashsearch(query)
-    photo = await download_file(random.choice(photo), "last.jpg")
+    reply = await event.get_reply_message()
+    if reply and media_type(reply) == "Photo":
+        photo = await app.download_media(reply.media)
+    else:
+        query = random.choice(["blur", "color", "blur background", "background", "neon", "neon lights", "lights", "light", "city", "car", "wallpaper"])
+        photo = await unsplashsearch(query)
+        photo = await download_file(random.choice(photo), "last.jpg")
     font = random.choice(glob.glob("userbot/other/fonts/*"))
     await event.reply(str(font))
     stroke_width = int(len(text))
     if stroke_width < 4:
-        return await event.edit("**• Please Enter A Text Larger Than 4 Word!**")
+        return await event.edit("**• Please Enter A Text Larger Than 3 Word!**")
     LogoMaker.make_logo(photo, text, font, "output.png", fill=random.choice(colors), stroke_width=stroke_width / 2, stroke_fill=random.choice(colors))
     await app.send_file(event.chat_id, "output.png", caption=f"""**• Logo By:** {DB.get_key("OWNER_NAME")}""")
     await event.delete()
