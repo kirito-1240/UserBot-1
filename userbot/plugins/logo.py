d6831c7c564b34b115f13d90c0318191ae095cc8
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-30 00:41:22 +0430

    Update logo.py

--- userbot/plugins/logo.py
+++ userbot/plugins/logo.py
@@ -10,11 +10,12 @@ async def googlesearch(event):
     await event.edit("`• Please Wait . . .`")
     text = str(event.pattern_match.group(1))
     colors = ["green", "blue", "red", "black", "white", "orange", "yellow", "purple", "pink", "gray"]
-    query = random.choice(["blur", "blur background", "background", "neon lights", "lights", "wallpaper"])
+    query = random.choice(["blur", "color", "blur background", "background", "neon", "neon lights", "lights", "light", "city", "car", "wallpaper"])
     photo = await unsplashsearch(query)
     photo = await download_file(random.choice(photo), "last.jpg")
     font = random.choice(glob.glob("userbot/other/fonts/*"))
-    stroke_width = (int(len(text)) / 2) + 1
+    await event.reply(str(font))
+    stroke_width = (int(len(text)) / 2) + 2
     LogoMaker.make_logo(photo, text, font, "output.png", fill=random.choice(colors), stroke_width=stroke_width, stroke_fill=random.choice(colors))
     await app.send_file(event.chat_id, "output.png", caption=f"""**• Logo By:** {DB.get_key("OWNER_NAME")}""")
     await event.delete()
