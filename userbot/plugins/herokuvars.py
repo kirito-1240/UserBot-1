4f9f7e1e90201a78ec2ce51d34ad870be90407b8
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-31 01:15:06 +0430

    Create herokuvars.py

---
+++ userbot/plugins/herokuvars.py
@@ -0,0 +1,31 @@
+from userbot import app
+from userbot.events import alien
+from userbot.database import DB
+import Config, heroku3
+
+Heroku = heroku3.from_key(Config.HEROKU_API)
+APP = Heroku.apps()[Config.HEROKU_APP_NAME]
+
+@alien(pattern="setv (.*) | (.*)")
+async def set_var(event):
+    await event.edit("`• Please Wait . . .`")
+    key = str(event.pattern_match.group(1))
+    value = event.pattern_match.group(2)
+    CONFIG = app.config()
+    if key in CONFIG:
+        CONFIG[key] = value
+        await event.edit(f"**• Updated Var In Heroku:**\n\n**• Key:** ( `{key}` )\n**• Value:** ( `(value}` )")
+    else:
+        CONFIG[key] = value
+        await event.edit(f"**• Set New Var In Heroku:**\n\n**• Key:** ( `{key}` )\n**• Value:** ( `(value}` )")
+
+@alien(pattern="delv (.*)")
+async def set_var(event):
+    await event.edit("`• Please Wait . . .`")
+    key = str(event.pattern_match.group(1))
+    CONFIG = app.config()
+    if key in CONFIG:
+        await event.edit(f"**• Deleted Var From Heroku:**\n\n**• Key:** ( `{key}` )\n**• Value:** ( `(CONFIG[key]}` )")
+        del CONFIG[key]
+    else:
+        await event.edit(f"**• This Var Is Not In Heroku!**")
