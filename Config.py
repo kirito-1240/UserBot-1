137b04f52056b65961430f66a2f5a8c2f6f45b3e
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-30 21:35:47 +0430

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,6 +4,7 @@ API_ID = int(os.environ.get("API_ID")) or None
 API_HASH = os.environ.get("API_HASH", None)
 SESSION = os.environ.get("SESSION", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+COMMAND_HANDLER = os.environ.get("COMMAND_HANDLER", ".")
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
 HEROKU_API = os.environ.get("HEROKU_API", None)
 REDIS_URL = os.environ.get("REDIS_URL", None)
