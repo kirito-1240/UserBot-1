e946a5b9f08296bdbc0d7ee69921428e24cbfbfe
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-31 00:17:50 +0430

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -26,16 +26,12 @@ def alien(
     groups_only=False,
     privates_only=False,
     channels_only=False,
-    handler=None,
     outgoing=True,
     incoming=False,
     **kwargs,
 ):
-    if pattern and handler:
-        reg = "(?i)^\" + handler
-        pattern = re.compile(reg + pattern + "$")
-    elif pattern and Config.COMMAND_HANDLER:
-        reg = "(?i)^\" + Config.COMMAND_HANDLER
+    if pattern and Config.COMMAND_HANDLER:
+        reg = "(?i)^\\" + Config.COMMAND_HANDLER
         pattern = re.compile(reg + pattern + "$")
         
     def decorator(func):
