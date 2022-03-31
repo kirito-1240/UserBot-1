e985742323e418c71ce859bf9ac6c167c4b9dffd
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-01 00:49:31 +0430

    Update inline_help.py

--- userbot/assistant/inline_help.py
+++ userbot/assistant/inline_help.py
@@ -2,13 +2,22 @@ from userbot.events import alien_inline, alien_callback
 from userbot.utils import chunks
 from userbot.database import DB
 from telethon import Button
-import os, glob, re
+import os, glob, re, random
 from userbot.database import PLUGINS_HELP
 
+PIC = random.choice(DB.get_key("START_PIC"))
 
 @alien_inline("alien", owner=True)
 async def alien_help(event):
     files = glob.glob("userbot/plugins/*.py")
+    list = []
+    for file in sorted(files[0:10]):
+        name = str(os.path.basename(file).replace(".py" , ""))
+        list.append(Button.inline(f"• {name.title()} •", data=f"plugin_{name}_1"))
+    buttons = []
+    for key in chunks(list, 2):
+        buttons.append(key)
+    buttons.append([Button.inline("❌ Close ❌", data="close") , Button.inline("Next ▶️", data="page_2")])
     text = f"""
 **• Alien Userbot Help Menu!**
 
@@ -16,41 +25,18 @@ async def alien_help(event):
 **• Assistant:** @{DB.get_key("ASSISTANT_USERNAME")}
 
 **• Plugins Count:** ( `{len(files)}` )
+**• Page:** ( 1 )
 """
-    if len(files) > 10:
-        list = []
-        for file in sorted(files[0:10]):
-            name = str(os.path.basename(file).replace(".py" , ""))
-            list.append(Button.inline(f"• {name.title()} •", data=f"plugin_{name}_1"))
-        buttons = []
-        for key in chunks(list, 2):
-            buttons.append(key)
-        buttons.append([Button.inline(f"• Next •", data="page_2")])
-        text += "**• Page:** ( 1 )"
-        result = event.builder.article(
-            title="Alien Help Menu",
-            text=text,
-            buttons=buttons,
-        )
-        await event.answer([result])
-    else:
-        list = []
-        for file in sorted(files):
-            name = str(os.path.basename(file).replace(".py" , ""))
-            list.append(Button.inline(f"• {name} •", data=f"plugin_{name}_1"))
-        buttons = []
-        for key in chunks(list, 2):
-            buttons.append(key)
-        result = event.builder.article(
-            title="Alien Help Menu",
-            text=text,
-            buttons=buttons,
-        )
-        await event.answer([result])
+    result = event.builder.photo(
+        file=PIC,
+        title="Alien Help Menu",
+        text=text,
+        buttons=buttons,
+    )
+    await event.answer([result])
 
 @alien_callback(re.compile("page_(.*)"), owner=True)
 async def alien_help_pages(event):
-    await event.edit(str(event))
     data = int(event.pattern_match.group(1))
     files = glob.glob("userbot/plugins/*.py")
     start = int(f"{(data - 1)}0")
@@ -64,10 +50,13 @@ async def alien_help_pages(event):
     buttons = []
     for key in chunks(list, 2):
         buttons.append(key)
-    buttons.append([Button.inline(f"• Back •", data=f"page_{(data-1)}")])
-    buttons.append([Button.inline(f"• Close •", data="close")])
+    other = []    
+    if data != 1:
+        other.append(Button.inline(f"◀️ Back", data=f"page_{(data-1)}"))
+    other.append(Button.inline(f"❌ Close ❌", data="close"))
     if not end > len(files):
-        buttons.append([Button.inline(f"• Next •", data=f"page_{(data+1)}")])
+        other.append(Button.inline(f"Next ▶️", data=f"page_{(data+1)}"))
+    buttons.append([other])
     text = f"""
 **• Alien Userbot Help Menu!**
 
@@ -77,10 +66,9 @@ async def alien_help_pages(event):
 **• Plugins Count:** ( `{len(files)}` )
 **• Page:** ( {data} )
 """
-    await event.edit(text, buttons=buttons)
+    await event.edit(text, file=PIC, buttons=buttons)
 
-
-@alien_callback(re.compile("plugin(.*)_(.*)"), owner=True)
+@alien_callback(re.compile("plugin_(.*)_(.*)"), owner=True)
 async def alien_help_plugins(event):
     data = str(event.pattern_match.group(1))
     page = int(event.pattern_match.group(2))
