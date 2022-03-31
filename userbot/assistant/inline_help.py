baa52985be4879d8b4681eeee10f922050862785
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-31 23:56:25 +0430

    Update inline_help.py

--- userbot/assistant/inline_help.py
+++ userbot/assistant/inline_help.py
@@ -4,10 +4,6 @@ from userbot.database import DB
 from telethon import Button
 import os, glob
 
-@alien_inline("test_(.*)", owner=True)
-async def alien_help(event):
-    result = event.builder.article(title="Alien", text=str(event))
-    await event.answer([result])
 
 @alien_inline("alien_inline_help", owner=True)
 async def alien_help(event):
@@ -24,7 +20,7 @@ async def alien_help(event):
         list = []
         for file in sorted(files[0:10]):
             name = str(os.path.basename(file).replace(".py" , ""))
-            list.append(Button.inline(f"• {name} •", data=f"help_plugin_{name}"))
+            list.append(Button.inline(f"• {name.title()} •", data=f"help_plugin_{name}"))
         buttons = []
         for key in chunks(list, 2):
             buttons.append(key)
@@ -51,3 +47,31 @@ async def alien_help(event):
         )
         await event.answer([result])
 
+@alien_callback("alien_help_page_(.*)", owner=True)
+async def alien_help(event):
+    data = int(event.pattern_match.group(1))
+    files = glob.glob("userbot/plugins/*.py")
+    start = int(f"{data}0")
+    end = start + 10
+    if end > len(files):
+        end = len(files)
+    list = []
+    for file in sorted(files[start:end]):
+        name = str(os.path.basename(file).replace(".py" , ""))
+        list.append(Button.inline(f"• {name.title()} •", data=f"help_plugin_{name}"))
+    buttons = []
+    for key in chunks(list, 2):
+        buttons.append(key)
+    if not end > len(files):
+        buttons.append([Button.inline(f"• Next •", data=f"alien_help_page_{(data+1)}")])
+    buttons.append([Button.inline(f"• Back •", data=f"alien_help_page_{(data-1)}")])
+text = f"""
+**• Alien Userbot Help Menu!**
+
+**• Master:** {DB.get_key("OWNER")}
+**• Assistant:** @{DB.get_key("ASSISTANT_USERNAME")}
+
+**• Plugins Count:** ( `{len(files)}` )
+**• Page:** ( {data} )
+"""
+    await event.edit(text, buttons=buttons)
