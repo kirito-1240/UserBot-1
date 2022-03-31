4daaede93359a1ba494385d3ca420d99245e5290
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-01 00:09:46 +0430

    Update inline_help.py

--- userbot/assistant/inline_help.py
+++ userbot/assistant/inline_help.py
@@ -3,6 +3,7 @@ from userbot.utils import chunks
 from userbot.database import DB
 from telethon import Button
 import os, glob
+from userbot.database import PLUGINS_HELP
 
 
 @alien_inline("alien_inline_help", owner=True)
@@ -20,7 +21,7 @@ async def alien_help(event):
         list = []
         for file in sorted(files[0:10]):
             name = str(os.path.basename(file).replace(".py" , ""))
-            list.append(Button.inline(f"• {name.title()} •", data=f"help_plugin_{name}"))
+            list.append(Button.inline(f"• {name.title()} •", data=f"alien_help_plugin_{name}_1"))
         buttons = []
         for key in chunks(list, 2):
             buttons.append(key)
@@ -36,7 +37,7 @@ async def alien_help(event):
         list = []
         for file in sorted(files):
             name = str(os.path.basename(file).replace(".py" , ""))
-            list.append(Button.inline(f"• {name} •", data=f"help_plugin_{name}"))
+            list.append(Button.inline(f"• {name} •", data=f"alien_help_plugin_{name}_1"))
         buttons = []
         for key in chunks(list, 2):
             buttons.append(key)
@@ -48,7 +49,7 @@ async def alien_help(event):
         await event.answer([result])
 
 @alien_callback("alien_help_page_(.*)", owner=True)
-async def alien_help(event):
+async def alien_help_pages(event):
     data = int(event.pattern_match.group(1))
     files = glob.glob("userbot/plugins/*.py")
     start = int(f"{(data - 1)}0")
@@ -58,13 +59,14 @@ async def alien_help(event):
     list = []
     for file in sorted(files[start:end]):
         name = str(os.path.basename(file).replace(".py" , ""))
-        list.append(Button.inline(f"• {name.title()} •", data=f"help_plugin_{name}"))
+        list.append(Button.inline(f"• {name.title()} •", data=f"alien_help_plugin_{name}_{data}"))
     buttons = []
     for key in chunks(list, 2):
         buttons.append(key)
+    buttons.append([Button.inline(f"• Back •", data=f"alien_help_page_{(data-1)}")])
+    buttons.append([Button.inline(f"• Close •", data="alien_help_close")])
     if not end > len(files):
         buttons.append([Button.inline(f"• Next •", data=f"alien_help_page_{(data+1)}")])
-    buttons.append([Button.inline(f"• Back •", data=f"alien_help_page_{(data-1)}")])
     text = f"""
 **• Alien Userbot Help Menu!**
 
@@ -75,3 +77,9 @@ async def alien_help(event):
 **• Page:** ( {data} )
 """
     await event.edit(text, buttons=buttons)
+
+@alien_callback("alien_help_plugin_(.*)_(.*)", owner=True)
+async def alien_help_plugins(event):
+    data = str(event.pattern_match.group(1))
+    page = int(event.pattern_match.group(2))
+    await event.edit(f"{data} - {page}")
