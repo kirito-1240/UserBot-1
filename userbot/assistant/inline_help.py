ce20764e71f548f9236f2ff7a69d563a83d6901c
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-03-31 23:24:12 +0430

    Update inline_help.py

--- userbot/assistant/inline_help.py
+++ userbot/assistant/inline_help.py
@@ -1,32 +1,46 @@
 from userbot.events import alien_inline, alien_callback
-import os, glob
+from userbot.utils import chunks
+from userbot.database import DB
 from telethon import Button
-
-def split_list(List, index):
-    new_ = []
-    while List:
-        new_.extend([List[:index]])
-        List = List[index:]
-    return new_
+import os, glob
 
 @alien_inline("alien_inline_help", owner=True)
-async def help_func(event):
+async def alien_help(event):
     files = glob.glob("userbot/plugins/*.py")
-    plugin_list = []
-    for file in sorted(files):
-        name = os.path.basename(file).replace(".py" , "")
-        plugin_list.append(str(name))
-    count = len(plugin_list)
-    list = [
-        Button.inline(f"{key}", data=f"help_plugin_{key}")
-        for key in sorted(plugin_list[0:10])
-    ]
-    all = split_list(list, 2)
-    fl = split_list(all, 5)
-    fl.append([Button.inline("Next", data="page_1")])
-    result = event.builder.article(
-        title="Alien Help Menu",
-        text=str(count),
-        buttons=[fl],
-    )
-    await event.answer([result])
+    text = f"""
+**• Alien Userbot Help Menu!**
+
+**• Master:** {DB.get_key("OWNER")}
+**• Assistant:** {DB.get_key("ASSISTANT_USERNAME")}
+
+**• Plugins Count:** ( `{len(files)}` )
+"""
+    if len(files) > 15:
+        list = []
+        for file in sorted(files[0:10]):
+            name = str(os.path.basename(file).replace(".py" , ""))
+            list.append(Button.inline(f"• {name} •", data=f"help_plugin_{name}"))
+        buttons = []
+        for key in chunks(list, 2):
+            buttons.append(key)
+        buttons.append([Button.inline(f"• Next •", data=f"page_1")])
+        result = event.builder.article(
+            title="Alien Help Menu",
+            text=text,
+            buttons=buttons,
+        )
+        await event.answer([result])
+    else:
+        list = []
+        for file in sorted(files):
+            name = str(os.path.basename(file).replace(".py" , ""))
+            list.append(Button.inline(f"• {name} •", data=f"help_plugin_{name}"))
+        buttons = []
+        for key in chunks(list, 2):
+            buttons.append(key)
+        result = event.builder.article(
+            title="Alien Help Menu",
+            text=text,
+            buttons=buttons,
+        )
+        await event.answer([result])
