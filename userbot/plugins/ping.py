665a5ba2b128330b178cc1229f2583c4915a5362
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-01 01:13:00 +0430

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -12,3 +12,12 @@ async def ping(event):
     ms = (end - start).microseconds / 1000
     uptime = convert_time(time.time() - START_TIME)
     await event.edit(f"**• Pong!!** `{ms}`\n**• Uptime :** `{uptime}`")
+
+from userbot.database import PLUGINS_HELP
+name = (__name__).split(".")[-1]
+PLUGINS_HELP.update({
+    name:{
+        "info": "To Get Ping And Uptime!",
+        "commands": ["{cmdh}ping"],
+    }
+})
