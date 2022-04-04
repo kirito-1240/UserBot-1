bfd776fb98df5d159b1877ac594da6a52bb65d3a
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-05 00:25:15 +0430

    Update leavevc.py

--- userbot/vcbot/leavevc.py
+++ userbot/vcbot/leavevc.py
@@ -9,10 +9,10 @@ async def leavevc(event):
     chat = event.chat_id
     if player.group_call.is_connected:
         await ultSongs.group_call.stop()
-       if CLIENTS.get(chat):
+        if CLIENTS.get(chat):
             del CLIENTS[chat]
         if VIDEO_ON.get(chat):
             del VIDEO_ON[chat]
         await event.edit("**• Successfuly Leaved From Voice Chat In This Chat!**")
     else:
-        await event.delete()
+        await event.edit("**• Not On Voice Chat In This Chat!**")
