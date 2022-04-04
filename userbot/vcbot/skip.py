1bcd0c52f1d566c3abde8c7aac1291dcf28594cb
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-04 23:25:30 +0430

    Create skip.py

---
+++ userbot/vcbot/skip.py
@@ -0,0 +1,9 @@
+from userbot import app
+from userbot.events import alien
+from userbot.vcbot.helper import Player
+
+@alien(pattern="skip")
+async def skip_audio(event):
+    await event.edit("`• Please Wait . . .`")
+    player = Player(event.chat_id, event)
+    await player.play_from_queue()
