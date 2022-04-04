a2d987474f275af931c495881161f15e158d0884
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-05 00:06:29 +0430

    Create joinvc.py

---
+++ userbot/vcbot/joinvc.py
@@ -0,0 +1,13 @@
+from userbot import app
+from userbot.events import alien
+from userbot.vcbot.helper import Player
+
+@alien(pattern="joinvc")
+async def joinvc(event):
+    await event.edit("`• Please Wait . . .`")
+    player = Player(event.chat_id)
+    if not player.group_call.is_connected:
+        await player.vc_joiner
+        await event.edit("**• Successfuly Joined On Voice Chat In This Chat!**")
+    else:
+        await event.delete()
