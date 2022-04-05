2341dfbe0a6776f77794f7b0138ec1dfc9f7774e
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-06 01:17:22 +0430

    Update inline_calc.py

--- userbot/assistant/inline_calc.py
+++ userbot/assistant/inline_calc.py
@@ -1,13 +1,10 @@
 from userbot.events import alien_inline, alien_callback
 from telethon import Button
-from userbot.database import DB
-import os, glob, re, random, time
-import Config 
+import re
 
-@alien_inline("^alien_calc$", owner=True)
-async def help(event):
-    DB.set_key("ALIEN_CALC", "")
-    vars = [
+CALC = None
+
+vars = [
     "AC",
     "C",
     "⌫",
@@ -29,13 +26,17 @@ async def help(event):
     ".",
     "÷",
 ]
-    button = [Button.inline(f"{x}", data=f"calc_{x}") for x in vars]
-    buttons = list(zip(button[::4], button[1::4], button[2::4], button[3::4]))
-    buttons.append([Button.inline("=", data="calc=")])
+button = [Button.inline(f"{var}", data=f"calc{var}") for var in vars]
+buttons = list(zip(button[::4], button[1::4], button[2::4], button[3::4]))
+buttons.append([Button.inline("=", data="calc=")])
+
+@alien_inline("^alien_calc$", owner=True)
+async def calc_pattern(event):
+    CALC = None
     text = f"""
 **• Alien Userbot Calc Menu!**
 
-Your Calc:  ( `{DB.get_key("ALIEN_CALC")}` )
+**• Your Calc:** ( `{CALC  or "Empty"}` )
 """
     result = event.builder.article(
         title="Alien Calc Menu!",
@@ -43,3 +44,40 @@ Your Calc:  ( `{DB.get_key("ALIEN_CALC")}` )
         buttons=buttons,
     )
     await event.answer([result])
+
+@alien_callback(re.compile("calc(.*)"), owner=True)
+async def calc_callback(event):
+    work = str((event.pattern_match.group(1)).decode('utf-8'))
+    if work == "AC":
+        CALC = None
+    elif work == "C":
+        CALC = None
+    elif work == "⌫":
+        CALC = CALC[:-1]
+    elif work == "+":
+        CALC += "+"
+    elif work == "-":
+        CALC += "-"
+    elif work == "÷":
+        CALC += "/"
+    elif work == "x":
+        CALC += "*"
+    elif work == "%":
+        CALC += "/100"
+    elif work == "=":
+        if CALC.endswith(("*", ".", "/", "-", "+")):
+            CALC = CALC[:-1]
+        out = eval(CALC)
+        num = float(out)
+        text = f"""
+**• Alien Userbot Calc Menu!**
+
+**• Result:** ( `{num}` )
+"""
+        return await event.edit(text, buttons=buttons)
+    text = f"""
+**• Alien Userbot Calc Menu!**
+
+**• Your Calc:** ( `{CALC or "Empty"}` )
+"""
+    await event.edit(text, buttons=buttons)
