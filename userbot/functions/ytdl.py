9e40a177460ec396a6eb4cf32f2077d40a963af1
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-17 21:06:05 +0430

    Create ytdl.py

---
+++ userbot/functions/ytdl.py
@@ -0,0 +1,37 @@
+from yt_dlp import YoutubeDL
+
+def yt_formats(url):
+    info = YoutubeDL().extract_info(url, download=False)
+    video = audio = {
+        "title": info["title"],
+        "id": info["id"],
+        "duration": info["duration"],
+        "description": info["description"],
+        "formats": [],
+    }
+    for res in info["formats"]:  
+        if str(res["ext"]) == "mp4" or str(res["ext"]) == "mkv":
+            video["formats"].append(
+                    {
+                "url": res["url"],
+                "format": res["format"],
+                "format_note": res["format_note"],
+                "size": res["filesize"],
+                "format_id": res["format_id"],
+                "ext": res["ext"],
+                "resolution": res["resolution"],
+            }
+        )
+        elif str(res["ext"]) == "mp3" or str(res["ext"]) == "m4a":
+            audio["formats"].append(
+                    {
+                "url": res["url"],
+                "format": res["format"],
+                "format_note": res["format_note"],
+                "size": res["filesize"],
+                "format_id": res["format_id"],
+                "ext": res["ext"],
+                "resolution": res["resolution"],
+            }
+        )
+    return video, audio
