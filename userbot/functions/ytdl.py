dded29d3c9b7e7ee64f78fadf99fc00fe603dab6
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-19 03:20:02 +0430

    Update ytdl.py

--- userbot/functions/ytdl.py
+++ userbot/functions/ytdl.py
@@ -2,21 +2,43 @@ from yt_dlp import YoutubeDL
 
 def yt_video_down(url, format, filename):
     opts = {
-        "format": format,
-        "addmetadata": True,
-        "key": "FFmpegMetadata",
-        "writethumbnail": True,
-        "prefer_ffmpeg": True,
-        "geo_bypass": True,
-        "nocheckcertificate": True,
-        "postprocessors": [
-             {"key": "FFmpegVideoConvertor", "preferedformat": "mp4"},
-             {"key": "FFmpegMetadata"},
-        ],
-        "outtmpl": filename,
-        "logtostderr": False,
-        "quiet": True,
-    }
+            "format": f"{format}+bestaudio",
+            "addmetadata": True,
+            "key": "FFmpegMetadata",
+            "prefer_ffmpeg": True,
+            "geo_bypass": True,
+            "ignore_errors": True,
+            "nocheckcertificate": True,
+            "postprocessors": [
+                {"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}
+            ],
+            "outtmpl": filename,
+            "logtostderr": False,
+            "quiet": True,
+        }
+    with YoutubeDL(opts) as ytdl:
+        ytdl.download([url])
+
+def yt_audio_down(url, format, filename):
+    opts = {
+            "format": "bestaudio",
+            "addmetadata": True,
+            "key": "FFmpegMetadata",
+            "prefer_ffmpeg": True,
+            "geo_bypass": True,
+            "ignore_errors": True,
+            "audio_quality": format,
+            "nocheckcertificate": True,
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                }
+            ],
+            "outtmpl": filename,
+            "logtostderr": False,
+            "quiet": True,
+        }
     with YoutubeDL(opts) as ytdl:
         ytdl.download([url])
 
