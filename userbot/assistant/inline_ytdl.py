e8f9756e34d500f434f1fbdc6c4e4731081e176e
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-19 20:38:23 +0430

    Update inline_ytdl.py

--- userbot/assistant/inline_ytdl.py
+++ userbot/assistant/inline_ytdl.py
@@ -13,26 +13,19 @@ import math
 INFO = """
 **• Title:** ( `{}` )
 **• Description:** ( `{}` )
-**• Duration:** ( `{}` )
-**• Quality:** ( `{}` )
-**• Resolution:** ( `{}` )
-"""
-LASTINFO = """
-**• Title:** ( `{}` )
-**• Description:** ( `{}` )
 """
 
 @alien_inline(re.compile("ytdl_(.*)"), owner=True)
 async def ytdl(event):
     link = str(event.pattern_match.group(1))
     info = yt_info(link)
-    desc = (info["description"])[:500] + " ..."
+    desc = (info["description"])[:300] + " ..."
     thumb = info["title"] + ".jpg"
     await download_file(info["thumbnail"], thumb)
     buttons = [[Button.inline("🎞 Video 🎞", data=f"ytdown||video||{link}"), Button.inline("🎵 Audio 🎵", data=f"ytdown||audio||{link}")]]
     result = event.builder.photo(
         file=thumb,
-        text="{}\n\n**• Please Chose Mode To Download!**".format(LASTINFO.format(info["title"], desc)),
+        text="{}\n\n**• Please Chose Mode To Download!**".format(INFO.format(info["title"], desc)),
         buttons=buttons,
     )
     await event.answer([result])
@@ -43,18 +36,18 @@ async def ytdown(event):
     link = str(event.pattern_match.group(2).decode('utf-8'))
     await event.edit("`• Downloading . . .`")
     info = yt_info(link)
-    desc = (info["description"])[:200] + " ..."
+    desc = (info["description"])[:300] + " ..."
     thumb = info["title"] + ".jpg"
     if type == "video":
         filename = info["title"] + ".mp4"
         yt_video_down(link, filename)
-        await app.send_file(event.chat_id, filename, thumb=thumb, caption=INFO.format(info["title"], desc, convert_time(info["duration"]), vid["format_note"], vid["resolution"]))
+        await app.send_file(event.chat_id, filename, thumb=thumb, caption=INFO.format(info["title"], desc))
         os.remove(filename)
         os.remove(thumb)
         await app.delete_messages(event.chat_id, [event.id])
     elif type == "audio":
         filename = info["title"] + ".mp3"
         yt_video_down(link, filename)
-        await app.send_file(event.chat_id, filename, thumb=thumb, caption=INFO.format(info["title"], desc, convert_time(info["duration"]), aud["format_note"], aud["resolution"]))
+        await app.send_file(event.chat_id, filename, thumb=thumb, caption=INFO.format(info["title"], desc))
         os.remove(filename)
         os.remove(thumb)
