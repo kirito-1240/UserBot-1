15cc8c479347c167ee4d9c740100e242cdd38aa1
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-20 00:50:20 +0430

    Update ytdl.py

--- userbot/plugins/ytdl.py
+++ userbot/plugins/ytdl.py
@@ -1,9 +1,12 @@
 from userbot import app , bot
 from userbot.events import alien
+import re
 
 @alien(pattern="ytdl (.*)")
 async def ytdl(event):
     await event.edit("`• Please Wait . . .`")
+    if not re.search("/(?:https?:\/\/)?(?:www\.)?youtu(?:\.be\/|be.com\/\S*(?:watch|embed)(?:(?:(?=\/[-a-zA-Z0-9_]{11,}(?!\S))\/)|(?:\S*v=|v\/)))([-a-zA-Z0-9_]{11,})/gm", str(event.text)):
+        return await event.edit("**• Your Link Is Invalid!**")
     link = str(event.pattern_match.group(1))
     me = await bot.get_me()
     results = await app.inline_query(me.username, f"ytdl_{link}")
