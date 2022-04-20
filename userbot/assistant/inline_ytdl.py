c0095940dc3b684615ac4b0607e2f93f0eeddb05
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-20 16:10:57 +0430

    Update inline_ytdl.py

--- userbot/assistant/inline_ytdl.py
+++ userbot/assistant/inline_ytdl.py
@@ -46,7 +46,7 @@ async def ytdl(event):
         )
         for aud in list
     ])
-    buttons = zip(buttons[::2], buttons[1::2])
+    buttons = (buttons[::2], buttons[1::2])
     result = event.builder.photo(
         file=thumb,
         text="{}\n\n**• Please Chose Mode To Download!**".format(INFO.format(info["title"], link, info["view_count"], info["uploader"], desc)),
