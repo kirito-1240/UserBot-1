96ff24d91e68e69a2d7ca35ec9f1072706b710c4
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-20 13:02:06 +0430

    Create core.py

---
+++ userbot/functions/core.py
@@ -0,0 +1,24 @@
+import time
+import math
+from userbot.utils import convert_time, convert_bytes
+
+async def progress(current, total, event, start, type, filename):
+    if type == "d":
+        act = "• Downloading . . ."
+    elif type == "u":
+        act = "• Uploading . . ."
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elaptime = round(diff) * 1000
+        tcom = round((total - current) / speed) * 1000
+        eta = convert_time(elaptime + tcom)
+        com = "".join(["▰" for i in range(math.floor(percentage / 15))])
+        rem = "".join(["▱" for i in range(15 - math.floor(percentage / 15))])
+        percent = round(percentage, 2)
+        frem = convert_bytes(current)
+        ftotal = convert_bytes(total)
+        text = "`{}`\n**• File Name:** ( `{}` )\n\n`{}{}` - `{}%`\n\n**• Size:** ( `{}` **Of** `{}` )\n**• ETA:** ( `{}` )"
+        await event.edit(text.format(act, filename, com, rem , percent, frem, ftotal, eta))
