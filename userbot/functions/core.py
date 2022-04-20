e230ad8a3269f17fb8ecb3ee0eb5bdadb4e2fe37
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-20 13:14:21 +0430

    Update core.py

--- userbot/functions/core.py
+++ userbot/functions/core.py
@@ -15,9 +15,9 @@ async def progress(current, total, event, start, type, filename):
         elaptime = round(diff) * 1000
         tcom = round((total - current) / speed) * 1000
         eta = convert_time(elaptime + tcom)
-        com = "".join(["●" for i in range(math.floor(percentage / 20))])
+        com = "●".join(["●" for i in range(math.floor(percentage / 5))])
         percent = round(percentage, 2)
         frem = convert_bytes(current)
         ftotal = convert_bytes(total)
         text = "`{}`\n\n**• File Name:** ( `{}` )\n\n`[{}]` - `{}%`\n\n**• Size:** ( `{}` **Of** `{}` )\n\n**• ETA:** ( `{}` )"
-        await event.edit(text.format(act, filename, com, rem , percent, frem, ftotal, eta))
+        await event.edit(text.format(act, filename, com , percent, frem, ftotal, eta))
