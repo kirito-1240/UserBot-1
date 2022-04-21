1636e1d1661b12b05f6521da6afba500d543f1f9
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-21 20:51:10 +0430

    Update ytdl.py

--- userbot/plugins/ytdl.py
+++ userbot/plugins/ytdl.py
@@ -6,11 +6,13 @@ import re
 @alien(pattern="ytdl (.*)")
 async def ytdl(event):
     await event.edit("`• Please Wait . . .`")
-    if not re.search("((?:https|http)?:\/\/)?(?:www\.)?youtu(?:\.be\/|be.com\/\S*(?:watch|embed)(?:(?:(?=\/[-a-zA-Z0-9_]{10,}(?!\S))\/)|(?:\S*v=|v\/)))([-a-zA-Z0-9_]{10,})", str(event.text)):
-        return await event.edit("**• Your Link Is Invalid!**")
     link = str(event.pattern_match.group(1))
-    me = await bot.get_me()
-    results = await app.inline_query(me.username, f"ytdl_{link}")
-    click = await results[0].click(event.chat_id)
-    DB.set_key("YOUTUBE_GET_INLINE", f"{event.chat_id}||{click.id}")
-    await event.delete()
+    if match := re.match('(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/(shorts/|watch\?v=|embed/|v/|.+\?v=)?(?P<id>[A-Za-z0-9\-=_]{11})(.*)', link):
+        me = await bot.get_me()
+        link = "https://youtu.be/" + match["id"]
+        results = await app.inline_query(me.username, f"ytdl_{link}")
+        click = await results[0].click(event.chat_id)
+        DB.set_key("YOUTUBE_GET_INLINE", f"{event.chat_id}||{click.id}")
+        await event.delete()
+    else:
+        await event.edit("**• Your Link Is Invalid!**")
