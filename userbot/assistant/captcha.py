24b06695cc0eb5662189ea52e18cce1493321a67
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-23 20:33:25 +0430

    Update captcha.py

--- userbot/assistant/captcha.py
+++ userbot/assistant/captcha.py
@@ -9,15 +9,16 @@ import re, os
 async def send_captcha(event):
     strings = (string.ascii_letters + str(string.digits))
     buttons = []
+    user = await event.get_user()
     truetext = ""
     for x in range(6):
         truetext += random.choice(strings)
-    buttons.append(Button.inline(truetext, data=f"true||{event.sender_id}"))
+    buttons.append(Button.inline(truetext, data=f"true||{user.id}"))
     for i in range(11):
         falsetext = ""
         for x in range(6):
             falsetext += random.choice(strings)
-        buttons.append(Button.inline(falsetext, data=f"false||{event.sender_id}"))
+        buttons.append(Button.inline(falsetext, data=f"false||{user.id}"))
     buttons = (buttons[::4], buttons[1::4], buttons[2::4], buttons[3::4])
     await bot.send_message(event.chat_id, f"Hello {truetext}", buttons=buttons)
 
