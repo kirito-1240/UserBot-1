9469e1a47dc5944a442d809e99aa6a6c6e40f579
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-24 01:20:10 +0430

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -1,5 +1,7 @@
 from userbot import app, bot
 from userbot.core.logger import LOGS
+from userbot.utils import runcmd
+from userbot.functions.core import paste
 from time import gmtime, strftime
 from userbot.database import DB
 from traceback import format_exc
@@ -90,19 +92,22 @@ def alien(
                 await asyncio.sleep(e.seconds + 5)
             except:
                 date = strftime("%Y-%m-%d - %H:%M:%S", gmtime())
-                ftext = "**• Alien Userbot Logs •**\n\n"
-                ftext += f"**• Date:** `{date}`\n"
-                ftext += f"**• Chat ID:** `{event.chat_id}`\n"
-                ftext += f"**• Sender ID:** `{event.sender_id}`\n\n"
-                ftext += f"**• Event Trigger:**\n `{event.text}`\n\n"
-                ftext += f"**• Traceback Info:**\n `{format_exc()}`\n\n"
-                ftext += f"**• Error Text:**\n `{sys.exc_info()[1]}`"
+                ftext = f"• Alien Userbot Logs •\n\n"
+                ftext += f"• Date: {date}\n"
+                ftext += f"• Chat ID: {event.chat_id}\n"
+                ftext += f"• Sender ID: {event.sender_id}\n\n"
+                ftext += f"• Event Trigger:\n {event.text}\n\n"
+                ftext += f"• Traceback Info:\n {format_exc()}\n\n"
+                ftext += f"• Error Text:\n {sys.exc_info()[1]}\n\n"
                 try:
-                    await event.edit("`• Sorry, Alien Userbot Has Crashed. The Error Logs Are Stored In The Alien Userbot Log Group!`")
+                    await event.edit("• Sorry, Alien Userbot Has Crashed. The Error Logs Are Stored In The Alien Userbot Log Group!")
                 except:
                     pass
-                ope
-                await event.client.send_message(DB.get_key("LOG_GROUP"), ftext)
+                gilogs = (await runcmd('git log --pretty=format:"%an: %s" -50'))[0]
+                ftext += f"• Last Commits: {gilogs}"
+                paste = paste(ftext)
+                text = f"** • Alien Userbot Logs:** ( {paste['url']} )"
+                await event.client.send_message(DB.get_key("LOG_GROUP"), text)
         app.add_event_handler(wrapper, events.MessageEdited(pattern=pattern, **kwargs))
         app.add_event_handler(wrapper, events.NewMessage(pattern=pattern, **kwargs))
         return wrapper
