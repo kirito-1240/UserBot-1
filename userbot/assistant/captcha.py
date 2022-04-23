3e1d46b235d4553e9d52b636cdf2fbba3a6c0883
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-23 21:11:09 +0430

    Update captcha.py

--- userbot/assistant/captcha.py
+++ userbot/assistant/captcha.py
@@ -4,7 +4,7 @@ from userbot.database import DB
 from userbot.events import alien_callback
 from telethon import Button
 from telethon import events
-import re, os, glob
+import re, os, glob, asyncio
 os.system("pip install captcha")
 from captcha.image import ImageCaptcha
 
@@ -23,26 +23,28 @@ async def send_captcha(event):
     truetext = ""
     for x in range(6):
         truetext += random.choice(strings)
-    buttons.append(Button.inline(truetext, data=f"true||{user.id}"))
+    buttons.append(Button.inline(truetext, data=f"captcha||true||{user.id}"))
     for i in range(11):
         falsetext = ""
         for x in range(6):
             falsetext += random.choice(strings)
-        buttons.append(Button.inline(falsetext, data=f"false||{user.id}"))
+        buttons.append(Button.inline(falsetext, data=f"captcha||false||{user.id}"))
     buttons = (buttons[::4], buttons[1::4], buttons[2::4], buttons[3::4])
-    font = random.choice(glob.glob("userbot/other/fonts/*.ttf"))
+    font = random.choice(glob.glob("userbot/other/fonts/*"))
     image = ImageCaptcha(fonts=[font])
     image.write(truetext, f"captcha{event.chat_id}{user.id}.png")
-    await bot.send_message(event.chat_id, f"Hello {user.first_name}", file=f"captcha{event.chat_id}{user.id}.png", buttons=buttons)
+    await event.reply(f"**• Hello {user.first_name}**\n\n**• Please Select The Correct Option:**", file=f"captcha{event.chat_id}{user.id}.png", buttons=random.shuffle(buttons))
 
-@alien_callback(re.compile("(.*)\|\|(.*)"), owner=True)
+@bot.on(events.CallbackQuery(data=re.compile("captcha\|\|(.*)\|\|(.*)")))
 async def call_captcha(event):
     type = str((event.pattern_match.group(1)).decode('utf-8'))
     user_id = int((event.pattern_match.group(2)).decode('utf-8'))
+    if event.sender_id != user_id:
+        return await event.answer("• This Is Not For You 😠")
     if type == "true":
-        await event.answer("True", alert=True)
-        await bot.edit_permissions(chat.id, user.id, send_messages=True)
+        await bot.edit_permissions(event.chat_id, user_id, send_messages=True)
         await event.delete()
     else:
-        await event.answer("False", alert=True)
+        await event.answer("• The Option Is Not Correct, You Are Kicked!", alert=True)
+        await asyncio.sleep(2)
         await bot.kick_participant(event.chat_id, user_id)
