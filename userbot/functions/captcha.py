af4299255c168c51ca467e8c702b849fca289f24
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-24 04:11:05 +0430

    Update captcha.py

--- userbot/functions/captcha.py
+++ userbot/functions/captcha.py
@@ -13,17 +13,17 @@ def Captcha():
         r = random.random()
         random.shuffle(supported_emojis, lambda: r)
         for i in range(9):
-                emoji_names.append(supported_emojis[i])
-                paste_image_list.append((os.path.join(DATA_DIR, emojis_index.get(supported_emojis[i]) + ".png")))
-            position = [(20, 20), (180, 20), (310, 20), (20, 90), (180, 90), (310, 90), (20, 160), (180, 160), (310, 160)]
+            emoji_names.append(supported_emojis[i])
+            paste_image_list.append((os.path.join("./", emojis_index.get(supported_emojis[i]) + ".png")))
+        position = [(20, 20), (180, 20), (310, 20), (20, 90), (180, 90), (310, 90), (20, 160), (180, 160), (310, 160)]
 
-            for i in range(len(paste_image_list)):
-                img = Image.open(paste_image_list[i]).rotate(random.randint(0, 360), resample=Image.BICUBIC, expand=True)
-                img.thumbnail((200, 200), Image.ANTIALIAS)
-                background.paste(img, (position[i]), img)
+        for i in range(len(paste_image_list)):
+            img = Image.open(paste_image_list[i]).rotate(random.randint(0, 360), resample=Image.BICUBIC, expand=True)
+            img.thumbnail((200, 200), Image.ANTIALIAS)
+            background.paste(img, (position[i]), img)
                 
-            emoji_captcha_path = os.path.join("cache", "cap.png")
-            background.save(emoji_captcha_path, "PNG", quality=100)
-            return {"answer": emoji_names, "captcha": emoji_captcha_path, "is_error": False}
-        except Exception as e:
-            return {"is_error": True}
+        emoji_captcha_path = os.path.join("cache", "cap.png")
+        background.save(emoji_captcha_path, "PNG", quality=100)
+        return {"answer": emoji_names, "captcha": emoji_captcha_path, "is_error": False}
+    except Exception as e:
+        return {"is_error": True}
