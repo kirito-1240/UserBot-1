5f041829b16c681243ea1ba5ff76068e64600504
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-25 01:16:35 +0430

    Update github.py

--- userbot/functions/github.py
+++ userbot/functions/github.py
@@ -1,31 +1,37 @@
 from github import Github
 
 class GITAPP:
-    def __init__(token, repo):
-        g = Github(token)
-        repo = g.get_repo(repo)
-    def get_all_files
-all_files = []
-contents = repo.get_contents("")
-while contents:
-    file_content = contents.pop(0)
-    if file_content.type == "dir":
-        contents.extend(repo.get_contents(file_content.path))
-    else:
-        file = file_content
-        all_files.append(str(file).replace('ContentFile(path="','').replace('")',''))
+    def __init__(self, token, repo):
+        self.g = Github(token)
+        self.repo = self.g.get_repo(repo)
 
-for file in glob.glob("./emojis/*"):
+    def get_all_files(self, dir):
+        all_files = []
+        contents = self.repo.get_contents(dir)
+        while contents:
+            file_content = contents.pop(0)
+            if file_content.type == "dir":
+                contents.extend(self.repo.get_contents(file_content.path))
+            else:
+                file = file_content
+                all_files.append(str(file).replace('ContentFile(path="','').replace('")',''))
+        return all_files
 
-    with open(file, 'rb') as file:
-        content = file.read()
+    def create(self, oldfile, newfile):
+        try:
+            content = open(oldfile, "r").read()
+        except:
+            content = open(oldfile, "rb").read()
+        self.repo.create_file(newfile, "creating file", content, branch="master")
 
-    git_prefix = 'userbot/other/emojis'
-    git_file = git_prefix + file.split("/")[-1]
-    if git_file in all_files:
-        contents = repo.get_contents(git_file)
-        repo.update_file(contents.path, "committing files", content, contents.sha, branch="master")
-        print(git_file + ' UPDATED')
-    else:
-        repo.create_file(git_file, "committing files", content, branch="master")
-        print(git_file + ' CREATED')
+    def update(self, oldfile, newfile):
+        try:
+            content = open(oldfile, "r").read()
+        except:
+            content = open(oldfile, "rb").read()
+        contents = self.repo.get_contents(newfile)
+        self.repo.update_file(contents.path, "updating file", content, contents.sha, branch="master")
+
+    def delete(self, file):
+        contents = self.repo.get_contents(file, ref="master")
+        self.repo.delete_file(contents.path, "removing file", contents.sha, branch="master")
