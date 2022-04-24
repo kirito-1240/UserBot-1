c8795520c2d17ee83de657dd437b99a12495ab2e
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-24 16:41:37 +0430

    Delete test.py

--- userbot/assistant/test.py
+++
@@ -1,9 +0,0 @@
-from userbot import app
-from userbot.database import DB
-from telethon import events
-
-@app.on(events.Raw)
-async def handler(update):
-    if "UpdateChannel" in str(update):
-        open("event.txt", "w").write(str(update.stringify()))
-        await app.send_file(DB.get_key("LOG_GROUP"), file="event.txt")
