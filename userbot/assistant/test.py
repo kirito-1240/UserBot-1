cf36311a89be619f3381d8aa38aec2f17e3c5239
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-24 15:54:47 +0430

    Update test.py

--- userbot/assistant/test.py
+++ userbot/assistant/test.py
@@ -2,7 +2,7 @@ from userbot import app
 from userbot.database import DB
 from telethon import events
 
-@app.on(events.ChatAction)
+@app.on(events.ChatAction(func=lambda e : e.new_join_request))
 async def test(event):
     open("event.txt", "w").write(str(event))
     await app.send_file(DB.get_key("LOG_GROUP"), file="event.txt")
