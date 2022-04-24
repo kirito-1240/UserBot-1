dc6a73a636caf08a8ce6f6ac1ca48fa4f1648392
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-24 16:12:46 +0430

    Update test.py

--- userbot/assistant/test.py
+++ userbot/assistant/test.py
@@ -4,5 +4,6 @@ from telethon import events
 
 @app.on(events.Raw)
 async def handler(update):
-    open("event.txt", "w").write(str(update.stringify()))
-    await app.send_file(DB.get_key("LOG_GROUP"), file="event.txt")
+    if "UpdateChannel" in str(update):
+        open("event.txt", "w").write(str(update.stringify()))
+        await app.send_file(DB.get_key("LOG_GROUP"), file="event.txt")
