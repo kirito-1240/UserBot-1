3ce3b42fd4ae9a6932c82f33b68a2cbbefc058e5
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-24 20:09:31 +0430

    Update captcha.py

--- userbot/plugins/captcha.py
+++ userbot/plugins/captcha.py
@@ -11,9 +11,20 @@ async def set_type(event):
         if not event.chat_id in chats:
             chats.append(event.chat_id)
             DB.set_key("CAPTCHA_CHATS", chats)
-        await event.edit("**• Captcha Mode For This Chat Has Been Actived!**")
+        await event.edit("**• Captcha For This Chat Has Been Actived!**")
     else:
         if event.chat_id in chats:
             chats.remove(event.chat_id)
             DB.set_key("CAPTCHA_CHATS", chats)
-        await event.edit("**• Captcha Mode For This Chat Has Been DeActived!**")
+        await event.edit("**• Captcha For This Chat Has Been DeActived!**")
+
+@alien(pattern="captype (photo|text)")
+async def set_type(event):
+    await event.edit("`• Please Wait . . .`")
+    type = event.pattern_match.group(1)
+    if type.lower() == "photo":
+        DB.set_key("CAPTCHA_TYPE", "photo")
+        await event.edit("**• Captcha Mode Was Changed To Photo Mode!**")
+    else:
+        DB.set_key("CAPTCHA_TYPE", "text")
+        await event.edit("**• Captcha Mode Was Changed To Text Mode!**")
