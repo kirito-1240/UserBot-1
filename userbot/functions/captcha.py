67fbeb8142c8cb9ff26ee48dd219126ac13e6c9c
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-24 04:55:02 +0430

    Update captcha.py

--- userbot/functions/captcha.py
+++ userbot/functions/captcha.py
@@ -18,13 +18,13 @@ def Captcha():
     random.shuffle(supported_emojis, lambda: r)
     r = random.random()
     random.shuffle(supported_emojis, lambda: r)
-    for i in range(9):
+    for i in range(6):
         emoji_names.append(supported_emojis[i])
         paste_image_list.append((os.path.join(DATA_DIR, emojis_index.get(supported_emojis[i]) + ".png")))
-    position = [(20, 20), (180, 20), (310, 20), (20, 90), (180, 90), (310, 90), (20, 160), (180, 160), (310, 160)]
+    position = [(20, 20), (180, 20), (310, 20), (20, 160), (180, 160), (310, 160)]
     for i in range(len(paste_image_list)):
         img = Image.open(paste_image_list[i]).rotate(random.randint(0, 360), resample=Image.BICUBIC, expand=True)
-        img.thumbnail((100, 100), Image.ANTIALIAS)
+        img.thumbnail((200, 200), Image.ANTIALIAS)
         img.paste(img, (position[i]), img)
     emoji_captcha_path = os.path.join("cache", emojis_index.get(supported_emojis[i]) + ".png")
     img.save(emoji_captcha_path, "PNG")
