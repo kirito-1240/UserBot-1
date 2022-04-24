ef8c44644c47cadff8c845d49014d87811e7bb85
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-25 02:26:34 +0430

    Update captcha.py

--- userbot/functions/captcha.py
+++ userbot/functions/captcha.py
@@ -4,7 +4,9 @@ from PIL import Image
 from userbot.utils import shuffle
 from userbot.other.emoji import emojis_index
 from userbot.functions.helper import rand_string
-from Config import CURRENT_DIR
+from userbot.functions.tools import downloadfile
+from userbot.functions.github import GITAPP
+import Config
 
 def Captcha():
     new = Image.new('RGB', (430, 415), (random.randint(50, 200), random.randint(50, 200), random.randint(50, 200)))
@@ -14,16 +16,24 @@ def Captcha():
     emojis = shuffle(emojis)
     others = emojis
     for i in range(9):
-        others.remove(emojis[i])
-        emoji_names.append(emojis[i])
         index = emojis_index.get(emojis[i])
-        file = os.path.join(f"{CURRENT_DIR}/userbot/other/emojis/",  f"{index}.png")
+        file = os.path.join(f"{Config.CURRENT_DIR}/userbot/other/emojis/",  f"{index}.png")
+        if not os.path.exists(file)
+            link = "https://emoji.aranja.com/static/emoji-data/img-apple-160/{}.png".format(index)
+            try:
+                downloadfile(link, file)
+                gapp = GITAPP(Config.GIT_TOKEN, Config.REPO_NAME)
+                gapp.create(file, f"/userbot/other/emojis/{index}.png")
+            except:
+                countinue
+        emoji_names.append(emojis[i]) 
         pimages.append(file)
+        others.remove(emojis[i])
     position = [(20, 20), (160, 20), (300, 20), (20, 160), (160, 160), (300, 160), (20, 300), (160, 300), (300, 300)]
     for i in range(len(pimages)):
         img = Image.open(pimages[i]).rotate(random.randint(0, 360), resample=Image.BICUBIC, expand=True)
         img.thumbnail((100, 100), Image.ANTIALIAS)
         new.paste(img, (position[i]), img)
-    newpath = os.path.join(f"{CURRENT_DIR}/userbot/other/emojis/",  rand_string() + ".png")
+    newpath = os.path.join(f"{Config.CURRENT_DIR}/userbot/other/emojis/",  rand_string() + ".png")
     new.save(newpath, "PNG")
     return {"answer": emoji_names, "others": others, "captcha": newpath}
