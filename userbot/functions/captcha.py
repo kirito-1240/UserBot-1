f8fda2503d320047b60effae08586a90d467ced4
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-25 18:20:18 +0430

    Update captcha.py

--- userbot/functions/captcha.py
+++ userbot/functions/captcha.py
@@ -1,7 +1,5 @@
 from userbot.other.emojis_index import indexs
 from userbot.functions.helper import rand_string
-from userbot.functions.tools import downloadfile
-from userbot.functions.github import GITAPP
 from userbot.utils import shuffle
 from PIL import Image
 import random
@@ -9,9 +7,11 @@ import os
 import Config
 
 class Captcha:
-    def __init__(
+    def init(
         self,
         emojis=None,
+        rotate=True,
+        output=None,
     ):
         if emojis:
             self.emojis = emojis
@@ -26,18 +26,20 @@ class Captcha:
         others = emojis
         for i in range(9):
             index = indexs.get(emojis[i])
-            link = "https://emoji.aranja.com/static/emoji-data/img-apple-160/{}.png".format(index)
-            last = downloadfile(link, f"userbot/other/emojis/{index}.png")
+            file = os.path.join(f"{Config.CURRENT_DIR}/userbot/other/emojis/",  f"{index}.png")
+            if not os.path.exists(file):
+                continue
             emoji_names.append(emojis[i]) 
-            pimages.append(last)
+            pimages.append(file)
             others.remove(emojis[i])
         position = [(20, 20), (100, 20), (180, 20), (20, 100), (100, 100), (180, 100), (20, 180), (100, 180), (180, 180)]
         for i in range(len(pimages)):
-            img = Image.open(pimages[i]).rotate(random.randint(0, 360), resample=Image.BICUBIC, expand=True)
+            img = Image.open(pimages[i])
+            if rotate:
+                img.rotate(random.randint(0, 360), resample=Image.BICUBIC, expand=True)
             img.thumbnail((100, 100), Image.ANTIALIAS)
             new.paste(img, (position[i]), img)
-        newpath = os.path.join(f"{Config.CURRENT_DIR}/userbot/other/emojis/",  rand_string() + ".png")
+        outfile = output if output else rand_string() + ".png"
+        newpath = os.path.join(f"{Config.CURRENT_DIR}/userbot/other/emojis/",  outfile)
         new.save(newpath, "PNG")
-        for x in pimages:
-            os.remove(x)
         return {"answer": emoji_names, "others": others, "captcha": newpath}
