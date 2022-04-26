19c9ce09ecb65ccd5e16e3bdbf2672d6ed07e6a7
Abolfazl Farzin <96248859+MxAboli@users.noreply.github.com>
2022-04-26 20:45:43 +0430

    Update restart.py

--- userbot/plugins/restart.py
+++ userbot/plugins/restart.py
@@ -1,24 +1,12 @@
-from userbot import app
-from userbot.utils import restart_app , runcmd
 from userbot.events import alien
+from userbot.utils import restart_app
 from userbot.database import DB
-from userbot import Config
-import os
-import sys
 
 @alien(pattern="restart")
 async def restart(event):
-    one = "█"
-    two = "░"
-    for i in range(0 , 6):
-        c = 5 - int(i)
-        await event.edit(f"""`• Restarting {one*i}{two*c}`""")      
     await event.edit("**• Bot Restarted!**\n\n`• Please Wait For A Minutes . . .`")
     DB.set_key("RESTART" , f"{event.id}||{event.chat_id}")
-    if Config.HEROKU_API and Config.HEROKU_APP_NAME:
-        restart_app()
-    await runcmd("git pull")
-    os.execl(sys.executable, "python3", "-m", "userbot")
+    restart_app()
 
 from userbot.database import PLUGINS_HELP
 name = (__name__).split(".")[-1]
